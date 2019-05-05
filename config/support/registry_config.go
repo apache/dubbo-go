3c5015acbeafb1bf77cd6130a68f07bd1c037de7
vito.he <hxmhlt@163.com>
2019-05-05 10:53:04 +0800

    Add:2.6.0 add registry type

--- config/support/registry_config.go
+++ config/support/registry_config.go
@@ -13,6 +13,7 @@ import "github.com/dubbo/dubbo-go/config"
 type RegistryConfig struct {
 	Id         string `required:"true" yaml:"id"  json:"id,omitempty"`
 	TimeoutStr string `yaml:"timeout" default:"5s" json:"timeout,omitempty"` // unit: second
+	Type       string `required:"true" yaml:"type"  json:"type,omitempty"`
 	Group      string `yaml:"group" json:"group,omitempty"`
 	//for registry
 	Address string `yaml:"address" json:"address,omitempty"`
@@ -23,7 +24,7 @@ func loadRegistries(registriesIds []ConfigRegistry, registries []RegistryConfig,
 	for _, registry := range registriesIds {
 		for _, registryConf := range registries {
 			if string(registry) == registryConf.Id {
-				url, err := config.NewURL(context.TODO(), registryConf.Address, config.WithParams(registryConf.getUrlMap(roleType)))
+				url, err := config.NewURL(context.TODO(), registryConf.Address, config.WithParams(registryConf.getUrlMap(roleType)), config.WithProtocol(constant.REGISTRY_PROTOCOL))
 				if err != nil {
 					log.Error("The registry id:%s url is invalid ,and will skip the registry, error: %#v", registryConf.Id, err)
 				} else {
@@ -41,6 +42,7 @@ func (regconfig *RegistryConfig) getUrlMap(roleType config.RoleType) url.Values 
 	urlMap := url.Values{}
 	urlMap.Set(constant.GROUP_KEY, regconfig.Group)
 	urlMap.Set(constant.ROLE_KEY, roleType.String())
+	urlMap.Set(constant.REGISTRY_KEY, regconfig.Type)
 	urlMap.Set(constant.REGISTRY_TIMEOUT_KEY, regconfig.TimeoutStr)
 	return urlMap
 }
