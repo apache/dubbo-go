28a4d219983d33529336f71a79ac3c8d368066c1
vito.he <hxmhlt@163.com>
2019-05-15 16:38:46 +0800

    Add:unit test config

---
+++ config/mock_rpcservice.go
@@ -0,0 +1,19 @@
+package config
+
+import "context"
+
+type MockService struct {
+}
+
+func (*MockService) Service() string {
+	return "mockservice"
+}
+func (*MockService) Version() string {
+	return "1.0"
+}
+func (*MockService) GetUser(ctx context.Context, itf []interface{}, str *struct{}) error {
+	return nil
+}
+func (*MockService) GetUser1(ctx context.Context, itf []interface{}, str *struct{}) error {
+	return nil
+}
