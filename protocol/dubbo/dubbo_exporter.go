e19607d9ef08d2e815b4b5e4d4cc8c2292da6d72
fangyincheng <fangyincheng@sina.com>
2019-05-16 11:22:03 +0800

    Add:add serviceMap unregister

--- protocol/dubbo/dubbo_exporter.go
+++ protocol/dubbo/dubbo_exporter.go
@@ -5,6 +5,12 @@ import (
 )
 
 import (
+	log "github.com/AlexStocks/log4go"
+)
+
+import (
+	"github.com/dubbo/go-for-apache-dubbo/common"
+	"github.com/dubbo/go-for-apache-dubbo/common/constant"
 	"github.com/dubbo/go-for-apache-dubbo/protocol"
 )
 
@@ -15,5 +21,14 @@ type DubboExporter struct {
 func NewDubboExporter(key string, invoker protocol.Invoker, exporterMap *sync.Map) *DubboExporter {
 	return &DubboExporter{
 		BaseExporter: *protocol.NewBaseExporter(key, invoker, exporterMap),
+	}
+}
+
+func (de *DubboExporter) Unexport() {
+	service := de.GetInvoker().GetUrl().GetParam(constant.INTERFACE_KEY, "")
+	de.BaseExporter.Unexport()
+	err := common.ServiceMap.UnRegister(DUBBO, service)
+	if err != nil {
+		log.Error("[DubboExporter.Unexport] error: %v", err)
 	}
 }
