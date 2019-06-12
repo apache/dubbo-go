51651d685aca79dd56ca3ef9e975b4336086724a
zonghaishang <yiji@apache.org>
2019-06-12 20:10:11 +0800

    merge from master.

--- cluster/loadbalance/least_active.go
+++ cluster/loadbalance/least_active.go
@@ -18,9 +18,9 @@ import (
 )
 
 import (
-	"github.com/dubbo/go-for-apache-dubbo/cluster"
-	"github.com/dubbo/go-for-apache-dubbo/common/extension"
-	"github.com/dubbo/go-for-apache-dubbo/protocol"
+	"github.com/apache/dubbo-go/cluster"
+	"github.com/apache/dubbo-go/common/extension"
+	"github.com/apache/dubbo-go/protocol"
 )
 
 const (
