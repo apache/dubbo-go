645092f7787c6ef4b05e0a3e3348248c3f9b498d
YGrylls <910357107@qq.com>
2019-07-18 15:14:50 +0800

    wrong spell

--- filter/impl/default_hystrix_fallback.go
+++ filter/impl/default_hystrix_fallback.go
@@ -5,7 +5,7 @@ import (
 	"github.com/apache/dubbo-go/protocol"
 )
 
-func defalutHystrixFallback(err error, invoker protocol.Invoker, invocation protocol.Invocation, cb hystrix.CircuitBreaker) protocol.Result {
+func defaultHystrixFallback(err error, invoker protocol.Invoker, invocation protocol.Invocation, cb hystrix.CircuitBreaker) protocol.Result {
 	//By default, return nil value and the error occurred
 	res := &protocol.RPCResult{}
 	res.SetError(err)
