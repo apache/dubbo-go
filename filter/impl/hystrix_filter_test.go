67bae9a5975717c4355d8cb0b46f7c6a1b2f919b
YGrylls <910357107@qq.com>
2019-07-24 18:20:00 +0800

    Overhaul: Fix concurrency problems; Add support for server end; Add error whitelist(omit) TODO: Make downgrade a standalone filter, so it can offer more freedom for users and avoid extensions on extensions TODO: Add a new example

--- filter/impl/hystrix_filter_test.go
+++ filter/impl/hystrix_filter_test.go
@@ -17,17 +17,16 @@
 package impl
 
 import (
+	"regexp"
 	"testing"
 	"time"
 )
 import (
-	"github.com/afex/hystrix-go/hystrix"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 import (
 	"github.com/apache/dubbo-go/protocol"
-	"github.com/apache/dubbo-go/protocol/dubbo"
 	"github.com/apache/dubbo-go/protocol/invocation"
 )
 
@@ -37,36 +36,38 @@ func init() {
 
 func mockInitHystrixConfig() {
 	//Mock config
-	conf = &HystrixFilterConfig{
-		make(map[string]*CommandConfigWithFallback),
+	confConsumer = &HystrixFilterConfig{
+		make(map[string]*CommandConfigWithError),
 		"Default",
 		make(map[string]ServiceHystrixConfig),
 	}
-	conf.Configs["Default"] = &CommandConfigWithFallback{
+	confConsumer.Configs["Default"] = &CommandConfigWithError{
 		1000,
-		10,
-		20,
+		500,
+		18,
 		5000,
 		50,
-		"",
+		nil,
 	}
-	conf.Configs["userp"] = &CommandConfigWithFallback{
+	confConsumer.Configs["userp"] = &CommandConfigWithError{
 		2000,
 		8,
 		15,
 		4000,
 		45,
-		"",
+		nil,
 	}
-	conf.Configs["userp_m"] = &CommandConfigWithFallback{
+	confConsumer.Configs["userp_m"] = &CommandConfigWithError{
 		1200,
 		12,
 		5,
 		6000,
 		60,
-		"exampleFallback",
+		[]string{
+			"exception",
+		},
 	}
-	conf.Services["com.ikurento.user.UserProvider"] = ServiceHystrixConfig{
+	confConsumer.Services["com.ikurento.user.UserProvider"] = ServiceHystrixConfig{
 		"userp",
 		map[string]string{
 			"GetUser": "userp_m",
@@ -76,66 +77,39 @@ func mockInitHystrixConfig() {
 }
 
 func TestGetHystrixFilter(t *testing.T) {
-	filterGot := GetHystrixFilter()
+	filterGot := GetHystrixFilterConsumer()
 	assert.NotNil(t, filterGot)
 }
 
-type MockFallback struct {
-}
-
-func (m *MockFallback) FallbackFunc(err error, invoker protocol.Invoker, invocation protocol.Invocation, cb hystrix.CircuitBreaker) protocol.Result {
-	return &protocol.RPCResult{Rest: "MOCK"}
-}
-
 func TestGetConfig_1(t *testing.T) {
-	configGot := getConfig("com.ikurento.user.UserProvider", "GetUser")
+	configGot := getConfig("com.ikurento.user.UserProvider", "GetUser", true)
 	assert.NotNil(t, configGot)
 	assert.Equal(t, 1200, configGot.Timeout)
 	assert.Equal(t, 12, configGot.MaxConcurrentRequests)
 	assert.Equal(t, 6000, configGot.SleepWindow)
 	assert.Equal(t, 60, configGot.ErrorPercentThreshold)
 	assert.Equal(t, 5, configGot.RequestVolumeThreshold)
-	assert.Equal(t, "exampleFallback", configGot.Fallback)
 }
 
 func TestGetConfig_2(t *testing.T) {
-	configGot := getConfig("com.ikurento.user.UserProvider", "GetUser0")
+	configGot := getConfig("com.ikurento.user.UserProvider", "GetUser0", true)
 	assert.NotNil(t, configGot)
 	assert.Equal(t, 2000, configGot.Timeout)
 	assert.Equal(t, 8, configGot.MaxConcurrentRequests)
 	assert.Equal(t, 4000, configGot.SleepWindow)
 	assert.Equal(t, 45, configGot.ErrorPercentThreshold)
 	assert.Equal(t, 15, configGot.RequestVolumeThreshold)
-	assert.Equal(t, "", configGot.Fallback)
 }
 
 func TestGetConfig_3(t *testing.T) {
 	//This should use default
-	configGot := getConfig("Mock.Service", "GetMock")
+	configGot := getConfig("Mock.Service", "GetMock", true)
 	assert.NotNil(t, configGot)
 	assert.Equal(t, 1000, configGot.Timeout)
 	assert.Equal(t, 10, configGot.MaxConcurrentRequests)
 	assert.Equal(t, 5000, configGot.SleepWindow)
 	assert.Equal(t, 50, configGot.ErrorPercentThreshold)
 	assert.Equal(t, 20, configGot.RequestVolumeThreshold)
-	assert.Equal(t, "", configGot.Fallback)
-}
-
-func TestGetHystrixFallback(t *testing.T) {
-	fallback["mock"] = &MockFallback{}
-	fallbackGot := getHystrixFallback("mock")
-	assert.NotNil(t, fallbackGot)
-	fallbackGot = getHystrixFallback("notExist")
-	assert.IsType(t, &DefaultHystrixFallback{}, fallbackGot)
-}
-
-func TestDefaultHystrixFallback_FallbackFunc(t *testing.T) {
-	cb, _, _ := hystrix.GetCircuit("newCB")
-	defaultFallback := &DefaultHystrixFallback{}
-	result := defaultFallback.FallbackFunc(errors.Errorf("error"), &dubbo.DubboInvoker{}, nil, *cb)
-	assert.NotNil(t, result)
-	assert.Error(t, result.Error())
-
 }
 
 type testMockSuccessInvoker struct {
@@ -155,12 +129,12 @@ type testMockFailInvoker struct {
 
 func (iv *testMockFailInvoker) Invoke(invocation protocol.Invocation) protocol.Result {
 	return &protocol.RPCResult{
-		Err: errors.Errorf("Error"),
+		Err: errors.Errorf("exception"),
 	}
 }
 
 func TestHystrixFilter_Invoke_Success(t *testing.T) {
-	hf := &HystrixFilter{&DefaultHystrixFallback{}}
+	hf := &HystrixFilter{}
 	result := hf.Invoke(&testMockSuccessInvoker{}, &invocation.RPCInvocation{})
 	assert.NotNil(t, result)
 	assert.NoError(t, result.Error())
@@ -168,31 +142,14 @@ func TestHystrixFilter_Invoke_Success(t *testing.T) {
 }
 
 func TestHystrixFilter_Invoke_Fail(t *testing.T) {
-	hf := &HystrixFilter{&DefaultHystrixFallback{}}
+	hf := &HystrixFilter{}
 	result := hf.Invoke(&testMockFailInvoker{}, &invocation.RPCInvocation{})
 	assert.NotNil(t, result)
 	assert.Error(t, result.Error())
 }
 
-type testHystrixFallback struct {
-}
-
-func (d *testHystrixFallback) FallbackFunc(err error, invoker protocol.Invoker, invocation protocol.Invocation, cb hystrix.CircuitBreaker) protocol.Result {
-	if cb.IsOpen() {
-		return &protocol.RPCResult{
-			//For the request is blocked due to the circuit breaker is open
-			Rest: true,
-		}
-	} else {
-		return &protocol.RPCResult{
-			//Circuit breaker not open
-			Rest: false,
-		}
-	}
-}
-
 func TestHystricFilter_Invoke_CircuitBreak(t *testing.T) {
-	hf := &HystrixFilter{&testHystrixFallback{}}
+	hf := &HystrixFilter{COrP: true}
 	resChan := make(chan protocol.Result, 50)
 	for i := 0; i < 50; i++ {
 		go func() {
@@ -203,9 +160,30 @@ func TestHystricFilter_Invoke_CircuitBreak(t *testing.T) {
 	time.Sleep(time.Second * 6)
 	var lastRest bool
 	for i := 0; i < 50; i++ {
-		lastRest = (<-resChan).Result().(bool)
+		lastRest = (<-resChan).Error().(*HystrixFilterError).CbOpen()
 	}
 	//Normally the last result should be true, which means the circuit has been opened
 	assert.True(t, lastRest)
+
+}
+
+func TestHystricFilter_Invoke_CircuitBreak_Omit_Exception(t *testing.T) {
+	reg, _ := regexp.Compile(".*exception.*")
+	regs := []*regexp.Regexp{reg}
+	hf := &HystrixFilter{res: regs, COrP: true}
+	resChan := make(chan protocol.Result, 50)
+	for i := 0; i < 50; i++ {
+		go func() {
+			result := hf.Invoke(&testMockFailInvoker{}, &invocation.RPCInvocation{})
+			resChan <- result
+		}()
+	}
+	time.Sleep(time.Second * 6)
+	var lastRest bool
+	for i := 0; i < 50; i++ {
+		lastRest = (<-resChan).Error().(*HystrixFilterError).CbOpen()
+	}
+
+	assert.False(t, lastRest)
 
 }
