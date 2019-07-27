306054c1f08ecf7e29460e0409ff20b0b5867ee1
vito.he <hxmhlt@163.com>
2019-07-27 21:34:34 +0800

    Mod: make simple more easy

--- examples/helloworld/dubbo/java-server/src/main/java/com/ikurento/user/UserProviderImpl.java
+++ examples/helloworld/dubbo/java-server/src/main/java/com/ikurento/user/UserProviderImpl.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UserProviderImpl implements UserProvider {
-    // private static final Logger logger = LoggerFactory.getLogger(getClass()); // 只输出到dubbo的log(logs/server.log)
     private static final Logger LOG = LoggerFactory.getLogger("UserLogger"); // 输出到user-server.log
     Map<String, User> userMap = new HashMap<String, User>();
 
