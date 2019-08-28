bea6d62bf8a97f6870dd226dab50d2f6bb276089
AlexStocks <alexstocks@foxmail.com>
2019-08-28 12:07:05 +0800

    Fix: delete AlexStocks author info in license

--- examples/general/dubbo/java-server/src/main/java/com/ikurento/user/UserProviderImpl.java
+++ examples/general/dubbo/java-server/src/main/java/com/ikurento/user/UserProviderImpl.java
@@ -23,8 +23,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Iterator;
 
-// import org.apache.log4j.Logger;
-// import org.apache.log4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
