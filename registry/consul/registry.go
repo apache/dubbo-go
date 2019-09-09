8bccdaa894659b5ac123bb88e3513d045451d9b2
vito.he <hxmhlt@163.com>
2019-09-09 14:14:18 +0800

    Mod:remove registry subscribe listen.next sleep error

--- registry/consul/registry.go
+++ registry/consul/registry.go
@@ -150,7 +150,6 @@ func (r *consulRegistry) Subscribe(url *common.URL, notifyListener registry.Noti
 			if serviceEvent, err := listener.Next(); err != nil {
 				logger.Warnf("Selector.watch() = error{%v}", perrors.WithStack(err))
 				listener.Close()
-				time.Sleep(time.Duration(RegistryConnDelay) * time.Second)
 				return
 			} else {
 				logger.Infof("update begin, service event: %v", serviceEvent.String())
