9848e0bd4da6dc8951a76eaa73a58dd7b8097bf9
aliiohs <rzy1107@163.com>
2019-09-10 15:03:34 +0800

    debug log

--- cluster/directory/static_directory.go
+++ cluster/directory/static_directory.go
@@ -59,6 +59,7 @@ func (dir *staticDirectory) List(invocation protocol.Invocation) []protocol.Invo
 	invokers := dir.invokers
 	localRouters := dir.routers
 	fmt.Println("========", len(localRouters))
+
 	if len(localRouters) > 0 {
 		for _, router := range localRouters {
 			if reflect.ValueOf(router.Url()).IsNil() || router.Url().GetParamBool(constant.RUNTIME_KEY, false) {
