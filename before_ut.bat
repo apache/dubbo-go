377850b3a076571cd8a90794b7cf031303251ad4
Joe Zou <yixian.zou@gmail.com>
2019-09-29 15:02:42 +0800

    change to curl

--- before_ut.bat
+++ before_ut.bat
@@ -1,5 +1,5 @@
 set zkJar=zookeeper-3.4.9-fatjar.jar
 md remoting\zookeeper\zookeeper-4unittest\contrib\fatjar config_center\zookeeper\zookeeper-4unittest\contrib\fatjar registry\zookeeper\zookeeper-4unittest\contrib\fatjar
-certutil.exe -urlcache -split -f https://github.com/dubbogo/resources/raw/master/zookeeper-4unitest/contrib/fatjar/%zkJar% remoting/zookeeper/zookeeper-4unittest/contrib/fatjar/%zkJar%
+curl -L https://github.com/dubbogo/resources/raw/master/zookeeper-4unitest/contrib/fatjar/%zkJar% -o remoting/zookeeper/zookeeper-4unittest/contrib/fatjar/%zkJar%
 xcopy /f "remoting/zookeeper/zookeeper-4unittest/contrib/fatjar/%zkJar%" "config_center/zookeeper/zookeeper-4unittest/contrib/fatjar/"
 xcopy /f "remoting/zookeeper/zookeeper-4unittest/contrib/fatjar/%zkJar%" "registry/zookeeper/zookeeper-4unittest/contrib/fatjar/"
