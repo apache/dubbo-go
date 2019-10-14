19c1f3aeb20c02019013cadba37da9edffafeb26
Ming Deng <mindeng@ebay.com>
2019-10-14 22:24:25 +0800

    Refactor MethodServiceTpsLimitImpl; Read config from yml

--- config/method_config.go
+++ config/method_config.go
@@ -25,12 +25,15 @@ import (
 )
 
 type MethodConfig struct {
-	InterfaceId   string
-	InterfaceName string
-	Name          string `yaml:"name"  json:"name,omitempty" property:"name"`
-	Retries       string `yaml:"retries"  json:"retries,omitempty" property:"retries"`
-	Loadbalance   string `yaml:"loadbalance"  json:"loadbalance,omitempty" property:"loadbalance"`
-	Weight        int64  `yaml:"weight"  json:"weight,omitempty" property:"weight"`
+	InterfaceId      string
+	InterfaceName    string
+	Name             string `yaml:"name"  json:"name,omitempty" property:"name"`
+	Retries          string `yaml:"retries"  json:"retries,omitempty" property:"retries"`
+	Loadbalance      string `yaml:"loadbalance"  json:"loadbalance,omitempty" property:"loadbalance"`
+	Weight           int64  `yaml:"weight"  json:"weight,omitempty" property:"weight"`
+	TpsLimitInterval string `yaml:"tps.limit.interval" json:"tps.limit.interval,omitempty" property:"tps.limit.interval"`
+	TpsLimitRate     string `yaml:"tps.limit.rate" json:"tps.limit.rate,omitempty" property:"tps.limit.rate"`
+	TpsLimitStrategy string `yaml:"tps.limit.strategy" json:"tps.limit.strategy,omitempty" property:"tps.limit.strategy"`
 }
 
 func (c *MethodConfig) Prefix() string {
