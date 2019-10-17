122629c86184eddb9ad54c7fa3acad6dfaee1587
Ming Deng <mindeng@ebay.com>
2019-10-17 20:24:58 +0800

    fix reveiw: rename the file

--- filter/impl/tps_limit_filter.go
+++ filter/impl/tps_limit_filter.go
@@ -21,6 +21,7 @@ import (
 	"github.com/apache/dubbo-go/common/constant"
 	"github.com/apache/dubbo-go/common/extension"
 	"github.com/apache/dubbo-go/filter"
+	_ "github.com/apache/dubbo-go/filter/impl/tps"
 	"github.com/apache/dubbo-go/protocol"
 )
 
