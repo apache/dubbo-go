add01dd5014dc3b100a5f0de51c9bdf6716ddce2
xujianhai666 <zero.xu@bytedance.com>
2019-10-23 15:22:57 +0800

    stash

---
+++ config_center/apollo/impl.go
@@ -0,0 +1,191 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apollo
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/apache/dubbo-go/common"
+	"github.com/apache/dubbo-go/common/constant"
+	"github.com/apache/dubbo-go/config_center"
+	"github.com/apache/dubbo-go/config_center/parser"
+	"github.com/apache/dubbo-go/remoting"
+
+	"github.com/zouyx/agollo"
+)
+
+const (
+	apolloEnvKey         = "env";
+	apolloAddrKey        = "apollo.meta";
+	apolloClusterKey     = "apollo.cluster";
+	apolloProtocolPrefix = "http://";
+)
+
+type apolloDynamicConfiguration struct {
+	url *common.URL
+
+	listeners sync.Map
+	appConf   *agollo.AppConfig
+	parser    parser.ConfigurationParser
+}
+
+func newApolloDynamicConfiguration(url *common.URL) (*apolloDynamicConfiguration, error) {
+	c := &apolloDynamicConfiguration{
+		url: url,
+	}
+	configEnv := url.GetParam(apolloEnvKey, "");
+	configAddr := c.getAddressWithProtocolPrefix(url)
+	configCluster := url.GetParam(constant.CONFIG_GROUP_KEY, "")
+	if len(configEnv) != 0 {
+		os.Setenv(apolloEnvKey, configEnv)
+	}
+
+	key := os.Getenv(apolloEnvKey)
+	if len(key) != 0 || constant.ANYHOST_VALUE == configAddr {
+		configAddr = key
+	}
+
+	appId := os.Getenv("app.id")
+	namespace := url.GetParam(constant.CONFIG_NAMESPACE_KEY, config_center.DEFAULT_GROUP)
+	readyConfig := &agollo.AppConfig{
+		AppId:         appId,
+		Cluster:       configCluster,
+		NamespaceName: namespace,
+		Ip:            configAddr,
+	}
+
+	agollo.InitCustomConfig(func() (*agollo.AppConfig, error) {
+		return readyConfig, nil
+	})
+
+	agollo.Start()
+
+	return c, nil
+}
+
+func (c *apolloDynamicConfiguration) start() {
+	for event := range agollo.ListenChangeEvent() {
+		for name, change := range event.Changes {
+			cfgChangeEvent := &config_center.ConfigChangeEvent{
+				Key:        name,
+				Value:      change.NewValue,
+				ConfigType: c.getChangeType(change.ChangeType),
+			}
+			c.listeners.Range(func(key, value interface{}) bool {
+				for listener, _ := range value.(apolloListener).listeners {
+					listener.Process(cfgChangeEvent)
+				}
+				return true
+			})
+		}
+	}
+}
+
+func (c *apolloDynamicConfiguration) getChangeType(change agollo.ConfigChangeType) remoting.EventType {
+	switch change {
+	case agollo.ADDED:
+		return remoting.EventTypeAdd
+	case agollo.DELETED:
+		return remoting.EventTypeDel
+	case agollo.MODIFIED:
+		return remoting.EventTypeUpdate
+	default:
+		panic("unknow type: " + strconv.Itoa(int(change)))
+	}
+}
+
+func (c *apolloDynamicConfiguration) AddListener(key string, listener config_center.ConfigurationListener, opts ...config_center.Option) {
+	k := &config_center.Options{}
+	for _, opt := range opts {
+		opt(k)
+	}
+
+	key = k.Group + key
+	l, _ := c.listeners.LoadOrStore(key, NewApolloListener())
+	l.(apolloListener).AddListener(listener)
+}
+
+func (c *apolloDynamicConfiguration) RemoveListener(key string, listener config_center.ConfigurationListener, opts ...config_center.Option) {
+	k := &config_center.Options{}
+	for _, opt := range opts {
+		opt(k)
+	}
+
+	key = k.Group + key
+	l, ok := c.listeners.Load(key)
+	if ok {
+		l.(apolloListener).RemoveListener(listener)
+	}
+}
+
+func (c *apolloDynamicConfiguration) GetConfig(key string, opts ...config_center.Option) (string, error) {
+	k := &config_center.Options{}
+	for _, opt := range opts {
+		opt(k)
+	}
+	group := k.Group
+	if len(group) != 0 && c.url.GetParam(constant.CONFIG_GROUP_KEY, config_center.DEFAULT_GROUP) != group {
+		readyConfig := &agollo.AppConfig{
+			AppId:         c.appConf.AppId,
+			Cluster:       c.appConf.Cluster,
+			NamespaceName: "application",
+			Ip:            c.appConf.Ip,
+		}
+
+		agollo.InitCustomConfig(func() (*agollo.AppConfig, error) {
+			return readyConfig, nil
+		})
+
+		agollo.Start()
+		//Config config = ConfigService.getAppConfig();
+		//return config.getProperty(key, null);
+	}
+	return agollo.GetStringValue(key, ""), nil
+}
+
+func (c *apolloDynamicConfiguration) getAddressWithProtocolPrefix(url *common.URL) string {
+	address := ""
+	converted := address
+	if len(address) != 0 {
+		parts := strings.Split(address, ",")
+		addrs := make([]string, 0)
+		for _, part := range parts {
+			addr := part
+			if !strings.HasPrefix(part, apolloProtocolPrefix) {
+				addr = apolloProtocolPrefix + part
+			}
+			addrs = append(addrs, addr)
+		}
+		converted = strings.Join(addrs, ",")
+	}
+	return converted
+}
+
+func (c *apolloDynamicConfiguration) Parser() parser.ConfigurationParser {
+	return c.parser
+}
+func (c *apolloDynamicConfiguration) SetParser(p parser.ConfigurationParser) {
+	c.parser = p
+}
+
+func (c *apolloDynamicConfiguration) GetConfigs(key string, opts ...config_center.Option) (string, error) {
+	return c.GetConfig(key, opts...)
+}
