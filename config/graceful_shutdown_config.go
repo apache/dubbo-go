96b6aecd3d0f3fd084097c39739e9b4adac7e211
Ming Deng <mindeng@ebay.com>
2019-11-10 21:39:45 +0800

    Fix BUG & DEBUG

--- config/graceful_shutdown_config.go
+++ config/graceful_shutdown_config.go
@@ -17,10 +17,59 @@
 
 package config
 
+import (
+	"strconv"
+	"time"
 
+	"github.com/apache/dubbo-go/common/logger"
+)
+
+const (
+	defaultTimeout  = 60 * time.Second
+	defaultStepTimeout = 10 * time.Second
+)
 type ShutdownConfig struct {
-	// it's the total timeout configuration. After that, the application will exit.
-	Timeout                       string `yaml:"timeout" json:"timeout,omitempty" property:"timout"`
-	AcceptNewRequestsTimeout      string `yaml:"requests.accept.timeout" json:"requests.accept.timeout,omitempty" property:"requests.accept.timeout"`
-	WaitingProcessRequestsTimeout string `yaml:"requests.reject.timeout" json:"requests.reject.timeout,omitempty" property:"requests.reject.timeout"`
+	/*
+	 * Total timeout. Even though we don't release all resources,
+	 * the application will shutdown if the costing time is over this configuration. The unit is ms.
+	 * default value is 60 * 1000 ms = 1 minutes
+	 * In general, it should be bigger than 3 * StepTimeout.
+	 */
+	Timeout string `yaml:"timeout" json:"timeout,omitempty" property:"timeout"`
+	/*
+	 * the timeout on each step. You should evaluate the response time of request
+	 * and the time that client noticed that server shutdown.
+	 * For example, if your client will received the notification within 10s when you start to close server,
+	 * and the 99.9% requests will return response in 2s, so the StepTimeout will be bigger than(10+2) * 1000ms,
+	 * maybe (10 + 2*3) * 1000ms is a good choice.
+	 */
+	StepTimeout string `yaml:"step_timeout" json:"step.timeout,omitempty" property:"step.timeout"`
+	// when we try to shutdown the application, we will reject the new requests. In most cases, you don't need to configure this.
+	RejectRequestHandler string `yaml:"reject_handler" json:"reject_handler,omitempty" property:"reject_handler"`
+	// true -> new request will be rejected.
+	RejectRequest bool
+
+	// true -> all requests had been processed. In provider side it means that all requests are returned response to clients
+	// In consumer side, it means that all requests getting response from servers
+	RequestsFinished bool
+}
+
+func (config *ShutdownConfig) GetTimeout() time.Duration {
+	result, err := strconv.ParseInt(config.Timeout, 0, 0)
+	if err != nil {
+		logger.Errorf("The Timeout configuration is invalid: %s, and we will use the default value: %s",
+			config.Timeout, defaultTimeout.String(), err)
+		return defaultTimeout
+	}
+	return time.Millisecond * time.Duration(result)
+}
+
+func (config *ShutdownConfig) GetStepTimeout() time.Duration {
+	result, err := strconv.ParseInt(config.StepTimeout, 0, 0)
+	if err != nil {
+		logger.Errorf("The StepTimeout configuration is invalid: %s, and we will use the default value: %s",
+			config.Timeout, defaultStepTimeout.String(), err)
+		return defaultStepTimeout
+	}
+	return time.Millisecond * time.Duration(result)
 }
