b15e8cd75a9befce8396a7a8c870600f8ae6cc01
skyitachi <skbysp@gmail.com>
2019-12-31 11:56:37 +0800

    remove some log

--- config/service.go
+++ config/service.go
@@ -18,7 +18,6 @@
 package config
 
 import (
-	"fmt"
 	"github.com/apache/dubbo-go/common"
 )
 
@@ -34,7 +33,6 @@ func SetConsumerService(service common.RPCService) {
 
 // SetProService is called by init() of implement of RPCService
 func SetProviderService(service common.RPCService) {
-	fmt.Println("register service ", service.Reference())
 	proServices[service.Reference()] = service
 }
 
