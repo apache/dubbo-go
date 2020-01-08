42beed7a66d9c7870e66bf96d1753ec1bf2b1b12
flycash <ming_flycash@qq.com>
2020-01-08 21:17:06 +0800

    Implementing Metered interface

--- metrics/metered.go
+++ metrics/metered.go
@@ -17,12 +17,41 @@
 
 package metrics
 
+import (
+	"time"
+)
+
 /**
  * It's the go version of com.alibaba.metrics.Metered
  */
 type Metered interface {
 	Metric
 	Counting
+	// Get the accurate number per collecting interval
+	// keyed by timestamp in milliseconds
 	GetInstantCount() map[int64]int64
+	// Get the accurate number per collecting interval since (including) the start time
+	// keyed by timestamp in milliseconds
 	GetInstantCountSince(startTime int64) map[int64]int64
+
+	// Get the collecting interval
+	GetInstantCountInterval() time.Duration
+
+	// Returns the fifteen-minute exponentially-weighted moving average rate at which events have
+	// occurred since the meter was created.
+	// This rate has the same exponential decay factor as the fifteen-minute load average in the `top` Unix command
+	GetFifteenMinuteRate() float64
+
+	// Returns the fifteen-minute exponentially-weighted moving average rate at which events have
+	// occurred since the meter was created.
+	// This rate has the same exponential decay factor as the five-minute load average in the `top` Unix command
+	GetFiveMinuteRate() float64
+
+	// Returns the mean rate at which events have occurred since the meter was created.
+	GetMeanRate() float64
+
+	// Returns the fifteen-minute exponentially-weighted moving average rate at which events have
+	// occurred since the meter was created.
+	// This rate has the same exponential decay factor as the one-minute load average in the `top` Unix command
+	GetOneMinuteRate() float64
 }
