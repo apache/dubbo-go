e0c29a578f29e823e6ccddeb8d433a1896aab5eb
amudong <xiaoxiong5288@gmail.com>
2020-01-12 04:29:14 +0800

    add nacos configcenter

---
+++ config_center/nacos/listener.go
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nacos
+
+import (
+	"context"
+)
+
+import (
+	"github.com/nacos-group/nacos-sdk-go/vo"
+)
+
+import (
+	"github.com/apache/dubbo-go/config_center"
+	"github.com/apache/dubbo-go/remoting"
+)
+
+func callback(ctx context.Context, listener config_center.ConfigurationListener, namespace, group, dataId, data string) {
+	listener.Process(&config_center.ConfigChangeEvent{Key: dataId, Value: data, ConfigType: remoting.EventTypeUpdate})
+}
+
+func (l *nacosDynamicConfiguration) addListener(key string, listener config_center.ConfigurationListener) {
+	_, loaded := l.keyListeners.Load(key)
+	if !loaded {
+		_, cancel := context.WithCancel(context.Background())
+		(*l.client.Client).ListenConfig(vo.ConfigParam{ //TODO 这个listen接口应该要有个context的
+		//(*l.client.Client).ListenConfigWithContext(ctx, vo.ConfigParam{
+				DataId:   key,
+			Group:    "dubbo",
+			OnChange: func(namespace, group, dataId, data string) {
+				//go callback(ctx, listener, namespace, group, dataId, data)
+				go callback(context.TODO(), listener, namespace, group, dataId, data)
+			},
+		})
+		newListener := make(map[config_center.ConfigurationListener]context.CancelFunc)
+		newListener[listener] = cancel
+		l.keyListeners.Store(key, newListener)
+	} else {
+		// TODO check goroutine
+	}
+}
+
+func (l *nacosDynamicConfiguration) removeListener(key string, listener config_center.ConfigurationListener) {
+	// TODO not supported in current go_nacos_sdk version
+	//listeners, loaded := l.keyListeners.Load(key)
+	//if loaded {
+	//	listenerMap := listeners.(map[config_center.ConfigurationListener]context.CancelFunc)
+	//	listenerMap[listener]()
+	//	delete(listeners.(map[config_center.ConfigurationListener]context.CancelFunc), listener)
+	//}
+}
