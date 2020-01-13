bb613817e610f0e7a573fad0992e74833784602b
flycash <ming_flycash@qq.com>
2020-01-13 13:55:20 +0800

    define TracingFilter

---
+++ filter/filter_impl/tracing_filter.go
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package filter_impl
+
+import (
+	"github.com/apache/dubbo-go/common/extension"
+	"github.com/apache/dubbo-go/filter"
+	"github.com/apache/dubbo-go/protocol"
+)
+
+const (
+	tracingFilterName = "tracing"
+)
+func init() {
+	extension.SetFilter(tracingFilterName, NewTracingFilter)
+}
+
+type TracingFilter struct {
+
+}
+
+func (tf *TracingFilter) Invoke(invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+	panic("implement me")
+}
+
+func (tf *TracingFilter) OnResponse(result protocol.Result, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+	panic("implement me")
+}
+
+var (
+	tracingFilterInstance *TracingFilter
+)
+
+func NewTracingFilter() filter.Filter{
+	if tracingFilterInstance == nil {
+		tracingFilterInstance = &TracingFilter{}
+	}
+	return tracingFilterInstance
+}
