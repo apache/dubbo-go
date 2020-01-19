3611b3824543f05d0bd91f8e5091b3d8b1b2d48a
Ooo0oO0o0oO <907709476@qq.com>
2020-01-19 16:23:38 +0800

    wrap the common auth logic

--- filter/filter_impl/auth/consumer_sign.go
+++ filter/filter_impl/auth/consumer_sign.go
@@ -18,20 +18,21 @@ func init() {
 	extension.SetFilter(constant.CONSUMER_SIGN_FILTER, getConsumerSignFilter)
 }
 
-func (filter *ConsumerSignFilter) Invoke(invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+func (csf *ConsumerSignFilter) Invoke(invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
 	logger.Infof("invoking ConsumerSign filter.")
 	url := invoker.GetUrl()
-	shouldSign := url.GetParamBool(constant.SERVICE_AUTH_KEY, false)
-	if shouldSign {
-		authenticator := extension.GetAuthenticator(url.GetParam(constant.AUTHENTICATOR_KEY, constant.DEFAULT_AUTHENTICATOR))
-		if err := authenticator.Sign(invocation, &url); err != nil {
-			panic(fmt.Sprintf("Sign for invocation %s # %s failed", url.ServiceKey(), invocation.MethodName()))
-		}
+
+	err := doAuthWork(&url, func(authenticator filter.Authenticator) error {
+		return authenticator.Sign(invocation, &url)
+	})
+	if err != nil {
+		panic(fmt.Sprintf("Sign for invocation %s # %s failed", url.ServiceKey(), invocation.MethodName()))
+
 	}
 	return invoker.Invoke(invocation)
 }
 
-func (filter *ConsumerSignFilter) OnResponse(result protocol.Result, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+func (csf *ConsumerSignFilter) OnResponse(result protocol.Result, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
 	return result
 }
 func getConsumerSignFilter() filter.Filter {
--- filter/filter_impl/auth/provider_auth.go
+++ filter/filter_impl/auth/provider_auth.go
@@ -15,23 +15,24 @@ func init() {
 	extension.SetFilter(constant.PROVIDER_AUTH_FILTER, getProviderAuthFilter)
 }
 
-func (filter *ProviderAuthFilter) Invoke(invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+func (paf *ProviderAuthFilter) Invoke(invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
 	logger.Infof("invoking providerAuth filter.")
 	url := invoker.GetUrl()
-	shouldAuth := url.GetParamBool(constant.SERVICE_AUTH_KEY, false)
-	if shouldAuth {
-		authenticator := extension.GetAuthenticator(url.GetParam(constant.AUTHENTICATOR_KEY, constant.DEFAULT_AUTHENTICATOR))
-		if err := authenticator.Authenticate(invocation, &url); err != nil {
-			logger.Infof("auth the request: %v occur exception, cause: %s", invocation, err.Error())
-			return &protocol.RPCResult{
-				Err: err,
-			}
+
+	err := doAuthWork(&url, func(authenticator filter.Authenticator) error {
+		return authenticator.Authenticate(invocation, &url)
+	})
+	if err != nil {
+		logger.Infof("auth the request: %v occur exception, cause: %s", invocation, err.Error())
+		return &protocol.RPCResult{
+			Err: err,
 		}
 	}
+
 	return invoker.Invoke(invocation)
 }
 
-func (filter *ProviderAuthFilter) OnResponse(result protocol.Result, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+func (paf *ProviderAuthFilter) OnResponse(result protocol.Result, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
 	return result
 }
 func getProviderAuthFilter() filter.Filter {
