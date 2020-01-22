547372f664be3c6ef4d8c02b7071939e9075997a
Patrick <dreamlike.sky@foxmail.com>
2020-01-22 18:49:47 +0800

    init rest exporter and modify rest invoker

--- protocol/rest/rest_interface/rest_server.go
+++ protocol/rest/rest_interface/rest_server.go
@@ -1,8 +1,13 @@
 package rest_interface
 
+import (
+	"github.com/apache/dubbo-go/common"
+	"github.com/apache/dubbo-go/protocol"
+)
+
 type RestServer interface {
-	Start()
-	Deploy()
-	Undeploy()
+	Start(url common.URL)
+	Deploy(invoker protocol.Invoker, restMethodConfig map[string]*RestMethodConfig)
+	Undeploy(restMethodConfig map[string]*RestMethodConfig)
 	Destory()
 }
