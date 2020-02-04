5210dc9b977e3103f9af900f4362492fde44b8cf
Joe Zou <yixian.zou@gmail.com>
2020-02-04 14:30:00 +0800

    fix review comment

--- cluster/router/condition/listenable_router.go
+++ cluster/router/condition/listenable_router.go
@@ -19,7 +19,6 @@ package condition
 
 import (
 	"fmt"
-	"github.com/apache/dubbo-go/common/constant"
 )
 
 import (
@@ -29,6 +28,7 @@ import (
 import (
 	"github.com/apache/dubbo-go/common"
 	"github.com/apache/dubbo-go/common/config"
+	"github.com/apache/dubbo-go/common/constant"
 	"github.com/apache/dubbo-go/common/logger"
 	"github.com/apache/dubbo-go/config_center"
 	"github.com/apache/dubbo-go/protocol"
