0b4712fe0cf151757046703c5e2a4dc2867170e8
Ooo0oO0o0oO <907709476@qq.com>
2020-02-04 12:28:43 +0800

    add some comments

--- filter/authenticator.go
+++ filter/authenticator.go
@@ -6,6 +6,10 @@ import (
 )
 
 type Authenticator interface {
+
+	// give a sign to request
 	Sign(protocol.Invocation, *common.URL) error
+
+	// verify the signature of the request is valid or not
 	Authenticate(protocol.Invocation, *common.URL) error
 }
