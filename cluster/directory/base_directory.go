1748dbc7645370bff5e560c0bd5768a2e3aa1599
Joe Zou <yixian.zou@gmail.com>
2020-02-10 16:56:19 +0800

    fix review comment

--- cluster/directory/base_directory.go
+++ cluster/directory/base_directory.go
@@ -39,8 +39,9 @@ var routerURLSet = gxset.NewSet()
 
 // BaseDirectory  Abstract implementation of Directory: Invoker list returned from this Directory's list method have been filtered by Routers
 type BaseDirectory struct {
-	url         *common.URL
-	destroyed   *atomic.Bool
+	url       *common.URL
+	destroyed *atomic.Bool
+	// this mutex for change the properties in BaseDirectory, like routerChain , destroyed etc
 	mutex       sync.Mutex
 	routerChain router.Chain
 }
