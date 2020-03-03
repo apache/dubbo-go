b8a9ec555d268cadb3138235cfc824e4395a12ad
fangyincheng <fangyincheng@sina.com>
2020-03-04 01:48:17 +0800

    revert

--- registry/directory/directory.go
+++ registry/directory/directory.go
@@ -142,9 +142,8 @@ func (dir *registryDirectory) refreshInvokers(res *registry.ServiceEvent) {
 			}
 
 			//dir.cacheService.EventTypeAdd(res.Path, dir.serviceTTL)
-			oldInvoker = dir.cacheInvoker(url)
+			dir.cacheInvoker(url)
 		case remoting.EventTypeDel:
-			//dir.cacheService.EventTypeDel(res.Path, dir.serviceTTL)
 			oldInvoker = dir.uncacheInvoker(url)
 			logger.Infof("selector delete service url{%s}", res.Service)
 		default:
