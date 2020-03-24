cd23859a99e884b096688a99c50f99ae777aa5a1
Joe Zou <yixian.zou@gmail.com>
2020-03-24 12:44:29 +0800

    fix script download problem

--- validate_license.sh
+++ validate_license.sh
@@ -16,7 +16,7 @@
 
 remoteJarUrl="https://github.com/dubbogo/resources/raw/master/tools"
 
-wget -P ${remoteJarUrl}
+wget ${remoteJarUrl}
 
 #more usage check: https://github.com/lsm-dev/license-header-checker
 tools/license/license-header-checker -a -r -i vendor  tools/license/license.txt . go
