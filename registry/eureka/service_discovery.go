58f50540294e59b2394c1692d4bc0c543db4c675
Joe Zou <yixian.zou@gmail.com>
2020-04-25 22:52:03 +0800

    add eureka discovery

--- registry/eureka/service_discovery.go
+++ registry/eureka/service_discovery.go
@@ -18,14 +18,24 @@
 package eureka
 
 import (
-	"github.com/apache/dubbo-go/common"
-	"github.com/apache/dubbo-go/common/constant"
-	"github.com/apache/dubbo-go/common/extension"
-	"github.com/apache/dubbo-go/registry"
-	perrors "github.com/apache/dubbo-go/vendor/github.com/pkg/errors"
+	"fmt"
+	"net"
+	"strings"
+)
+
+import (
 	gxset "github.com/dubbogo/gost/container/set"
 	gxpage "github.com/dubbogo/gost/page"
 	"github.com/hudl/fargo"
+	perrors "github.com/pkg/errors"
+)
+
+import (
+	"github.com/apache/dubbo-go/common"
+	"github.com/apache/dubbo-go/common/constant"
+	"github.com/apache/dubbo-go/common/extension"
+	"github.com/apache/dubbo-go/common/logger"
+	"github.com/apache/dubbo-go/registry"
 )
 
 // init will put the service discovery into extension
@@ -58,11 +68,19 @@ func (e *eurekaServiceDiscovery) Destroy() error {
 }
 
 func (e *eurekaServiceDiscovery) Register(instance registry.ServiceInstance) error {
-	panic("implement me")
+	err := e.eurekaConnection.RegisterInstance(e.toDeregisterInstance(instance))
+	if err != nil {
+		return perrors.WithMessage(err, "Could not unregister the instance. "+instance.GetServiceName())
+	}
+	return nil
 }
 
 func (e *eurekaServiceDiscovery) Update(instance registry.ServiceInstance) error {
-	panic("implement me")
+	err := e.Unregister(instance)
+	if err != nil {
+		return perrors.WithStack(err)
+	}
+	return e.Register(instance)
 }
 
 // Unregister will unregister the instance
@@ -75,53 +93,113 @@ func (e *eurekaServiceDiscovery) Unregister(instance registry.ServiceInstance) e
 }
 
 func (e *eurekaServiceDiscovery) GetDefaultPageSize() int {
-	panic("implement me")
+	return registry.DefaultPageSize
 }
 
 func (e *eurekaServiceDiscovery) GetServices() *gxset.HashSet {
-	panic("implement me")
+	e.eurekaConnection.SelectServiceURL()
+	services := e.eurekaConnection.ServiceUrls
+	res := gxset.NewSet()
+	for _, e := range services {
+		res.Add(e)
+	}
+	return res
 }
 
 func (e *eurekaServiceDiscovery) GetInstances(serviceName string) []registry.ServiceInstance {
-	panic("implement me")
+	instance, err := e.eurekaConnection.GetInstance(serviceName, e.group)
+	if err != nil {
+		logger.Errorf("Could not query the instances for service: " + serviceName + ", group: " + e.group)
+		return make([]registry.ServiceInstance, 0, 0)
+	}
+	res := make([]registry.ServiceInstance, 0, 1)
+	d := &registry.DefaultServiceInstance{
+		Id:          instance.Id(),
+		ServiceName: instance.App,
+		Host:        instance.IPAddr,
+		Port:        instance.Port,
+		Enable:      instance.PortEnabled,
+		Healthy:     true,
+	}
+
+	if fargo.UP != instance.Status {
+		d.Healthy = false
+	}
+
+	res = append(res, d)
+
+	return res
 }
 
 func (e *eurekaServiceDiscovery) GetInstancesByPage(serviceName string, offset int, pageSize int) gxpage.Pager {
-	panic("implement me")
+	all := e.GetInstances(serviceName)
+	res := make([]interface{}, 0, pageSize)
+	// could not use res = all[a:b] here because the res should be []interface{}, not []ServiceInstance
+	for i := offset; i < len(all) && i < offset+pageSize; i++ {
+		res = append(res, all[i])
+	}
+	return gxpage.New(offset, pageSize, res, len(all))
 }
 
 func (e *eurekaServiceDiscovery) GetHealthyInstancesByPage(serviceName string, offset int, pageSize int, healthy bool) gxpage.Pager {
-	panic("implement me")
+	all := e.GetInstances(serviceName)
+	res := make([]interface{}, 0, pageSize)
+
+	var (
+		i     = offset
+		count = 0
+	)
+	for i < len(all) && count < pageSize {
+		ins := all[i]
+		if ins.IsHealthy() == healthy {
+			res = append(res, all[i])
+			count++
+		}
+		i++
+	}
+	return gxpage.New(offset, pageSize, res, len(all))
 }
 
 func (e *eurekaServiceDiscovery) GetRequestInstances(serviceNames []string, offset int, requestedSize int) map[string]gxpage.Pager {
-	panic("implement me")
+	res := make(map[string]gxpage.Pager, len(serviceNames))
+	for _, name := range serviceNames {
+		res[name] = e.GetInstancesByPage(name, offset, requestedSize)
+	}
+	return res
 }
 
 func (e *eurekaServiceDiscovery) AddListener(listener *registry.ServiceInstancesChangedListener) error {
-	panic("implement me")
+	return nil
 }
 
 func (e *eurekaServiceDiscovery) DispatchEventByServiceName(serviceName string) error {
-	panic("implement me")
+	return e.DispatchEventForInstances(serviceName, e.GetInstances(serviceName))
 }
 
 func (e *eurekaServiceDiscovery) DispatchEventForInstances(serviceName string, instances []registry.ServiceInstance) error {
-	panic("implement me")
+	return e.DispatchEvent(registry.NewServiceInstancesChangedEvent(serviceName, instances))
 }
 
 func (e *eurekaServiceDiscovery) DispatchEvent(event *registry.ServiceInstancesChangedEvent) error {
-	panic("implement me")
+	return nil
 }
 
 // toDeregisterInstance will create new service discovery instance
 func newEurekaServiceDiscovery(url *common.URL) (registry.ServiceDiscovery, error) {
+	addresses := strings.Split(url.Location, ",")
+	serviceURLs := make([]string, 0, len(addresses))
+	for _, addr := range addresses {
+		ip, portStr, err := net.SplitHostPort(addr)
+		if err != nil {
+			return nil, perrors.WithMessagef(err, "split [%s] ", addr)
+		}
+		serviceURLs = append(serviceURLs, fmt.Sprintf("http://%s:%s/eureka/v2", ip, portStr))
+	}
 	config := fargo.Config{}
-	config.Eureka.ServerPort = 1999
+	config.Eureka.ServiceUrls = serviceURLs
 	eurekaConnection := fargo.NewConnFromConfig(config)
 	return &eurekaServiceDiscovery{
 		eurekaConnection: &eurekaConnection,
 		URL:              url,
-		//group:            url.GetParam(constant.NACOS_GROUP, defaultGroup),
 	}, nil
 }
