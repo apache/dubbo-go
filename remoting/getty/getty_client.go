5f82243b459a406ab409d0d6ee8af304fe046436
cvictory <shenglicao2@gmail.com>
2020-05-08 17:37:55 +0800

    refactor getty

--- remoting/getty/getty_client.go
+++ remoting/getty/getty_client.go
@@ -18,22 +18,19 @@
 package getty
 
 import (
-	"github.com/apache/dubbo-go/remoting"
-	"gopkg.in/yaml.v2"
 	"math/rand"
 	"time"
-)
 
-import (
+	"github.com/apache/dubbo-go/remoting"
 	"github.com/dubbogo/getty"
-	gxsync "github.com/dubbogo/gost/sync"
-	perrors "github.com/pkg/errors"
-)
+	"gopkg.in/yaml.v2"
 
-import (
+	gxsync "github.com/dubbogo/gost/sync"
+
 	"github.com/apache/dubbo-go/common"
 	"github.com/apache/dubbo-go/common/logger"
 	"github.com/apache/dubbo-go/config"
+	perrors "github.com/pkg/errors"
 )
 
 var (
@@ -98,11 +95,6 @@ func SetClientConf(c ClientConfig) {
 	setClientGrpool()
 }
 
-// GetClientConf ...
-func GetClientConf() ClientConfig {
-	return *clientConf
-}
-
 func setClientGrpool() {
 	if clientConf.GrPoolSize > 1 {
 		clientGrpool = gxsync.NewTaskPool(gxsync.WithTaskPoolTaskPoolSize(clientConf.GrPoolSize), gxsync.WithTaskPoolTaskQueueLength(clientConf.QueueLen),
@@ -114,19 +106,17 @@ func setClientGrpool() {
 type Options struct {
 	// connect timeout
 	ConnectTimeout time.Duration
-	// request timeout
-	//RequestTimeout time.Duration
 }
 
-//AsyncCallbackResponse async response for dubbo
-type AsyncCallbackResponse struct {
-	common.CallbackResponse
-	Opts      Options
-	Cause     error
-	Start     time.Time // invoke(call) start time == write start time
-	ReadStart time.Time // read start time, write duration = ReadStart - Start
-	Reply     interface{}
-}
+////AsyncCallbackResponse async response for dubbo
+//type AsyncCallbackResponse struct {
+//	common.CallbackResponse
+//	Opts      Options
+//	Cause     error
+//	Start     time.Time // invoke(call) start time == write start time
+//	ReadStart time.Time // read start time, write duration = ReadStart - Start
+//	Reply     interface{}
+//}
 
 // Client ...
 type Client struct {
@@ -137,8 +127,6 @@ type Client struct {
 	codec           remoting.Codec
 	responseHandler remoting.ResponseHandler
 	ExchangeClient  *remoting.ExchangeClient
-	//sequence atomic.Uint64
-	//pendingResponses *sync.Map
 }
 
 // NewClient ...
@@ -160,6 +148,7 @@ func (c *Client) SetExchangeClient(client *remoting.ExchangeClient) {
 func (c *Client) SetResponseHandler(responseHandler remoting.ResponseHandler) {
 	c.responseHandler = responseHandler
 }
+
 func (c *Client) Connect(url common.URL) {
 	initClient(url.Protocol)
 	c.conf = *clientConf
@@ -169,41 +158,15 @@ func (c *Client) Connect(url common.URL) {
 	c.codec = remoting.GetCodec(url.Protocol)
 	c.addr = url.Location
 }
+
 func (c *Client) Close() {
 	if c.pool != nil {
 		c.pool.close()
 	}
 	c.pool = nil
 }
-func (c *Client) Request(request *remoting.Request, timeout time.Duration, callback common.AsyncCallback, response *remoting.PendingResponse) error {
 
-	//p := &DubboPackage{}
-	//p.Service.Path = strings.TrimPrefix(request.svcUrl.Path, "/")
-	//p.Service.Interface = request.svcUrl.GetParam(constant.INTERFACE_KEY, "")
-	//p.Service.Version = request.svcUrl.GetParam(constant.VERSION_KEY, "")
-	//p.Service.Group = request.svcUrl.GetParam(constant.GROUP_KEY, "")
-	//p.Service.Method = request.method
-	//
-	//p.Service.Timeout = c.opts.RequestTimeout
-	//var timeout = request.svcUrl.GetParam(strings.Join([]string{constant.METHOD_KEYS, request.method + constant.RETRIES_KEY}, "."), "")
-	//if len(timeout) != 0 {
-	//	if t, err := time.ParseDuration(timeout); err == nil {
-	//		p.Service.Timeout = t
-	//	}
-	//}
-	//
-	//p.Header.SerialID = byte(S_Dubbo)
-	//p.Body = hessian.NewRequest(request.args, request.atta)
-	//
-	//var rsp *PendingResponse
-	//if ct != CT_OneWay {
-	//	p.Header.Type = hessian.PackageRequest_TwoWay
-	//	rsp = NewPendingResponse()
-	//	rsp.response = response
-	//	rsp.callback = callback
-	//} else {
-	//	p.Header.Type = hessian.PackageRequest
-	//}
+func (c *Client) Request(request *remoting.Request, timeout time.Duration, response *remoting.PendingResponse) error {
 
 	var (
 		err     error
@@ -225,11 +188,11 @@ func (c *Client) Request(request *remoting.Request, timeout time.Duration, callb
 		conn.close()
 	}()
 
-	if err = c.transfer(session, request, timeout, response); err != nil {
+	if err = c.transfer(session, request, timeout); err != nil {
 		return perrors.WithStack(err)
 	}
 
-	if !request.TwoWay || callback != nil {
+	if !request.TwoWay || response.Callback != nil {
 		return nil
 	}
 
@@ -242,98 +205,6 @@ func (c *Client) Request(request *remoting.Request, timeout time.Duration, callb
 
 	return perrors.WithStack(err)
 }
-
-// CallOneway call one way
-//func (c *Client) CallOneway(request *Request) error {
-//
-//	return perrors.WithStack(c.call(CT_OneWay, request, NewResponse(nil, nil), nil))
-//}
-//
-//// Call if @response is nil, the transport layer will get the response without notify the invoker.
-//func (c *Client) Call(request *Request, response *Response) error {
-//
-//	ct := CT_TwoWay
-//	if response.reply == nil {
-//		ct = CT_OneWay
-//	}
-//
-//	return perrors.WithStack(c.call(ct, request, response, nil))
-//}
-//
-//// AsyncCall ...
-//func (c *Client) AsyncCall(request *Request, callback common.AsyncCallback, response *Response) error {
-//
-//	return perrors.WithStack(c.call(CT_TwoWay, request, response, callback))
-//}
-//
-//func (c *Client) call(ct CallType, request *Request, response *Response, callback common.AsyncCallback) error {
-//
-//	p := &DubboPackage{}
-//	p.Service.Path = strings.TrimPrefix(request.svcUrl.Path, "/")
-//	p.Service.Interface = request.svcUrl.GetParam(constant.INTERFACE_KEY, "")
-//	p.Service.Version = request.svcUrl.GetParam(constant.VERSION_KEY, "")
-//	p.Service.Group = request.svcUrl.GetParam(constant.GROUP_KEY, "")
-//	p.Service.Method = request.method
-//
-//	p.Service.Timeout = c.opts.RequestTimeout
-//	var timeout = request.svcUrl.GetParam(strings.Join([]string{constant.METHOD_KEYS, request.method + constant.RETRIES_KEY}, "."), "")
-//	if len(timeout) != 0 {
-//		if t, err := time.ParseDuration(timeout); err == nil {
-//			p.Service.Timeout = t
-//		}
-//	}
-//
-//	p.Header.SerialID = byte(S_Dubbo)
-//	p.Body = hessian.NewRequest(request.args, request.atta)
-//
-//	var rsp *PendingResponse
-//	if ct != CT_OneWay {
-//		p.Header.Type = hessian.PackageRequest_TwoWay
-//		rsp = NewPendingResponse()
-//		rsp.response = response
-//		rsp.callback = callback
-//	} else {
-//		p.Header.Type = hessian.PackageRequest
-//	}
-//
-//	var (
-//		err     error
-//		session getty.Session
-//		conn    *gettyRPCClient
-//	)
-//	conn, session, err = c.selectSession(request.addr)
-//	if err != nil {
-//		return perrors.WithStack(err)
-//	}
-//	if session == nil {
-//		return errSessionNotExist
-//	}
-//	defer func() {
-//		if err == nil {
-//			c.pool.put(conn)
-//			return
-//		}
-//		conn.close()
-//	}()
-//
-//	if err = c.transfer(session, p, rsp); err != nil {
-//		return perrors.WithStack(err)
-//	}
-//
-//	if ct == CT_OneWay || callback != nil {
-//		return nil
-//	}
-//
-//	select {
-//	case <-getty.GetTimeWheel().After(c.opts.RequestTimeout):
-//		c.removePendingResponse(SequenceType(rsp.seq))
-//		return perrors.WithStack(errClientReadTimeout)
-//	case <-rsp.done:
-//		err = rsp.err
-//	}
-//
-//	return perrors.WithStack(err)
-//}
 
 func (c *Client) selectSession(addr string) (*gettyRPCClient, getty.Session, error) {
 	rpcClient, err := c.pool.getGettyRpcClient(addr)
@@ -349,49 +220,15 @@ func (c *Client) heartbeat(session getty.Session) error {
 	req.Event = true
 	resp := remoting.NewPendingResponse(req.Id)
 	remoting.AddPendingResponse(resp)
-	return c.transfer(session, req, 3*time.Second, resp)
+	return c.transfer(session, req, 3*time.Second)
 }
 
-func (c *Client) transfer(session getty.Session, request *remoting.Request, timeout time.Duration,
-	rsp *remoting.PendingResponse) error {
-	//sequence = c.sequence.Add(1)
-	//
-	//if pkg == nil {
-	//	pkg = &DubboPackage{}
-	//	pkg.Body = hessian.NewRequest([]interface{}{}, nil)
-	//	pkg.Body = []interface{}{}
-	//	pkg.Header.Type = hessian.PackageHeartbeat
-	//	pkg.Header.SerialID = byte(S_Dubbo)
-	//}
-	//pkg.Header.ID = int64(sequence)
-
-	// cond1
-	//if rsp != nil {
-	//	c.addPendingResponse(rsp)
-	//}
-
+func (c *Client) transfer(session getty.Session, request *remoting.Request, timeout time.Duration) error {
 	err := session.WritePkg(request, timeout)
-	if rsp != nil { // cond2
-		// cond2 should not merged with cond1. cause the response package may be returned very
-		// soon and it will be handled by other goroutine.
-		rsp.ReadStart = time.Now()
-	}
-
+	//if rsp != nil { // cond2
+	//	// cond2 should not merged with cond1. cause the response package may be returned very
+	//	// soon and it will be handled by other goroutine.
+	//	rsp.ReadStart = time.Now()
+	//}
 	return perrors.WithStack(err)
 }
-
-//
-//func (c *Client) addPendingResponse(pr *PendingResponse) {
-//	c.pendingResponses.Store(SequenceType(pr.seq), pr)
-//}
-//
-//func (c *Client) removePendingResponse(seq SequenceType) *PendingResponse {
-//	if c.pendingResponses == nil {
-//		return nil
-//	}
-//	if presp, ok := c.pendingResponses.Load(seq); ok {
-//		c.pendingResponses.Delete(seq)
-//		return presp.(*PendingResponse)
-//	}
-//	return nil
-//}
