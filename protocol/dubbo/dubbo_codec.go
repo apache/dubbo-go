6fb99ded5e6b84d0d1e2697be7d5fb66b4b8bd9d
cvictory <shenglicao2@gmail.com>
2020-05-09 14:21:02 +0800

    remove unused code, add license

--- protocol/dubbo/dubbo_codec.go
+++ protocol/dubbo/dubbo_codec.go
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dubbo
 
 import (
@@ -114,12 +130,6 @@ func (c *DubboCodec) EncodeRequest(request *remoting.Request) (*bytes.Buffer, er
 		panic(err)
 	}
 	p.Service.Timeout = time.Duration(timeout)
-	//var timeout = request.svcUrl.GetParam(strings.Join([]string{constant.METHOD_KEYS, request.method + constant.RETRIES_KEY}, "."), "")
-	//if len(timeout) != 0 {
-	//	if t, err := time.ParseDuration(timeout); err == nil {
-	//		p.Service.Timeout = t
-	//	}
-	//}
 
 	p.Header.SerialID = byte(S_Dubbo)
 	p.Header.ID = request.Id
@@ -321,13 +331,6 @@ func (c *DubboCodec) decodeResponse(data []byte) (*remoting.Response, int, error
 		rpcResult.Attrs = pkg.Body.(*hessian.Response).Attachments
 		rpcResult.Rest = pkg.Body.(*hessian.Response).RspObj
 	}
-
-	//h.conn.updateSession(session)
-	//pendingResponse := h.conn.pool.rpcClient.removePendingResponse(SequenceType(p.Header.ID))
-	//if pendingResponse == nil {
-	//	logger.Errorf("failed to get pending response context for response package %s", *p)
-	//	return
-	//}
 
 	return response, hessian.HEADER_LENGTH + pkg.Header.BodyLen, nil
 }
