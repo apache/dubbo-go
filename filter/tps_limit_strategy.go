751a47dc311e314c0fd796f873f51776514cfeb5
william feng <>
2020-06-03 23:59:18 +0800

    update the comments for TPS

--- filter/tps_limit_strategy.go
+++ filter/tps_limit_strategy.go
@@ -17,7 +17,7 @@
 
 package filter
 
-// TpsLimitStrategy ...
+// TpsLimitStrategy defines how to do the TPS limiting in method level. 
 /*
  * please register your implementation by invoking SetTpsLimitStrategy
  * "UserProvider":
@@ -37,7 +37,7 @@ type TpsLimitStrategy interface {
 	IsAllowable() bool
 }
 
-// TpsLimitStrategyCreator, the creator abstraction for TpsLimitStrategy
+// TpsLimitStrategyCreator is the creator abstraction for TpsLimitStrategy
 type TpsLimitStrategyCreator interface {
 	// Create will create an instance of TpsLimitStrategy
 	// It will be a little hard to understand this method.
