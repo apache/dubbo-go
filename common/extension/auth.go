bcc014ebef31d72fa5f8b96f73cdd78d6750862e
william feng <>
2020-06-06 22:16:29 +0800

    fixed minor grammar issues

--- common/extension/auth.go
+++ common/extension/auth.go
@@ -32,7 +32,7 @@ func SetAuthenticator(name string, fcn func() filter.Authenticator) {
 }
 
 // GetAuthenticator finds the Authenticator with @name
-// panic if not found
+// Panic if not found
 func GetAuthenticator(name string) filter.Authenticator {
 	if authenticators[name] == nil {
 		panic("authenticator for " + name + " is not existing, make sure you have import the package.")
