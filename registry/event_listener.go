91f8a05d07d422df9e3e52f669637c92905321cc
Joe Zou <yixian.zou@gmail.com>
2020-06-06 23:26:00 +0800

    add comment in all files of registry directory

--- registry/event_listener.go
+++ registry/event_listener.go
@@ -36,6 +36,7 @@ type ConditionalEventListener interface {
 	Accept(e Event) bool
 }
 
+// ServiceInstancesChangedListener is use for the Service Discovery Changed
 // TODO (implement ConditionalEventListener)
 type ServiceInstancesChangedListener struct {
 	ServiceName string
