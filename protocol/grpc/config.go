cd10b8232e69e0b9968b0c38f59a5e270f7a2354
xuxiaoliang <xuxiaoliang03411@Hellobike.com>
2020-06-10 00:41:37 +0800

    add public method 、struc comment

--- protocol/grpc/config.go
+++ protocol/grpc/config.go
@@ -22,44 +22,44 @@ import (
 )
 
 type (
-	// ServerConfig
+	// ServerConfig currently is empty struct,for future expansion
 	ServerConfig struct {
 	}
 
-	// ClientConfig
+	// ClientConfig wrap client call parameters
 	ClientConfig struct {
 		// content type, more information refer by https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
 		ContentSubType string `default:"proto" yaml:"content_sub_type" json:"content_sub_type,omitempty"`
 	}
 )
 
-// GetDefaultClientConfig ...
+// GetDefaultClientConfig return grpc client default call options
 func GetDefaultClientConfig() ClientConfig {
 	return ClientConfig{
-		ContentSubType: CODEC_PROTO,
+		ContentSubType: codecProto,
 	}
 }
 
-// GetDefaultServerConfig ...
+// GetDefaultServerConfig currently return empty struct,for future expansion
 func GetDefaultServerConfig() ServerConfig {
 	return ServerConfig{}
 }
 
-// GetClientConfig ...
+// GetClientConfig return grpc client custom call options
 func GetClientConfig() ClientConfig {
 	return ClientConfig{}
 }
 
-// clientConfig Validate ...
+// Validate check if custom config encoding is supported in dubbo grpc
 func (c *ClientConfig) Validate() error {
-	if c.ContentSubType != CODEC_JSON && c.ContentSubType != CODEC_PROTO {
+	if c.ContentSubType != codecJson && c.ContentSubType != codecProto {
 		return perrors.Errorf(" dubbo-go grpc codec currently only support proto、json, %s isn't supported,"+
 			" please check protocol content_sub_type config", c.ContentSubType)
 	}
 	return nil
 }
 
-// severConfig Validate ...
+// Validate currently return empty struct,for future expansion
 func (c *ServerConfig) Validate() error {
 	return nil
 }
