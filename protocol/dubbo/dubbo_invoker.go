5f13e7feb8694bb0b62efc24d2adb90a6dc87189
cvictory <shenglicao2@gmail.com>
2020-06-16 14:25:08 +0800

    fix: path is not in param

--- protocol/dubbo/dubbo_invoker.go
+++ protocol/dubbo/dubbo_invoker.go
@@ -98,7 +98,7 @@ func (di *DubboInvoker) Invoke(ctx context.Context, invocation protocol.Invocati
 
 	inv := invocation.(*invocation_impl.RPCInvocation)
 	// init param
-	inv.SetAttachments(constant.PATH_KEY, strings.TrimPrefix(di.GetUrl().Path, constant.PATH_SEPARATOR))
+	inv.SetAttachments(constant.PATH_KEY, di.GetUrl().GetParam(constant.INTERFACE_KEY, ""))
 	for _, k := range attachmentKey {
 		if v := di.GetUrl().GetParam(k, ""); len(v) > 0 {
 			inv.SetAttachments(k, v)
