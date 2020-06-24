0dc83ea30b500c1bd9a6c7b2895155fd3d7bad9b
willson-chen <willson.chenwx@gmail.com>
2020-06-24 15:56:37 +0800

    Add ANYHOST_KEY judgment

--- registry/base_configuration_listener.go
+++ registry/base_configuration_listener.go
@@ -100,7 +100,13 @@ func ToConfigurators(urls []*common.URL, f func(url *common.URL) config_center.C
 			configurators = []config_center.Configurator{}
 			break
 		}
-		//TODO:anyhost_key judage
+
+		var override map[string][]string = url.GetParams()
+		delete(override, constant.ANYHOST_KEY)
+		if len(override) == 0 {
+			configurators = configurators[:0]
+			continue
+		}
 		configurators = append(configurators, f(url))
 	}
 	return configurators
