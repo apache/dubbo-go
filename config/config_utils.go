b23f354bd400cdbefcdf560ecb87fa8bb1683831
william feng <>
2020-06-27 15:37:20 +0800

    use go fmt to update the format

--- config/config_utils.go
+++ config/config_utils.go
@@ -39,7 +39,7 @@ func mergeValue(str1, str2, def string) string {
 		str = "," + constant.DEFAULT_KEY + str
 	}
 	str = strings.TrimPrefix(strings.Replace(str, ","+constant.DEFAULT_KEY, ","+def, -1), ",")
-	return removeMinus(strings.Split(str, ","))	
+	return removeMinus(strings.Split(str, ","))
 }
 
 func removeMinus(strArr []string) string {
@@ -50,14 +50,14 @@ func removeMinus(strArr []string) string {
 	var minusStrArr []string
 	for _, v := range strArr {
 		if strings.HasPrefix(v, "-") {
-			minusStrArr = append(minusStrArr,v[1:])
+			minusStrArr = append(minusStrArr, v[1:])
 		} else {
 			normalStr += fmt.Sprintf(",%s", v)
 		}
 	}
 	normalStr = strings.Trim(normalStr, ",")
 	for _, v := range minusStrArr {
-		normalStr = strings.Replace(normalStr, v, "",1)
+		normalStr = strings.Replace(normalStr, v, "", 1)
 	}
 	reg := regexp.MustCompile("[,]+")
 	normalStr = reg.ReplaceAllString(strings.Trim(normalStr, ","), ",")
