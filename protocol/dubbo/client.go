279aaa2597e1c7071ea80029f817740f97a41c29
lihaowei <haoweili35@gmail.com>
2020-08-09 14:12:22 +0800

    add nolint

--- protocol/dubbo/client.go
+++ protocol/dubbo/client.go
@@ -193,7 +193,7 @@ type Response struct {
 	atta  map[string]string
 }
 
-// NewResponse create a new Response.
+// nolint
 func NewResponse(reply interface{}, atta map[string]string) *Response {
 	return &Response{
 		reply: reply,
