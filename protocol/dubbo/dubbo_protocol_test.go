1af15e7b7be9d34811dfe59054341238b37f2589
haohongfan <haohongfan@ling.ai>
2020-08-16 16:08:22 +0800

    fix: testDubboProtocol_Export exporterMap after 'Unexport' url key

--- protocol/dubbo/dubbo_protocol_test.go
+++ protocol/dubbo/dubbo_protocol_test.go
@@ -101,7 +101,7 @@ func TestDubboProtocol_Export(t *testing.T) {
 	assert.True(t, eq2)
 
 	// make sure exporterMap after 'Unexport'
-	_, ok := proto.(*DubboProtocol).ExporterMap().Load(url.ServiceKey())
+	_, ok := proto.(*DubboProtocol).ExporterMap().Load(url2.ServiceKey())
 	assert.True(t, ok)
 	exporter2.Unexport()
 	_, ok = proto.(*DubboProtocol).ExporterMap().Load(url2.ServiceKey())
