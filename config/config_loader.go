43c11f17e4384752a139c4353ba19f9683cc0b0f
Patrick <dreamlike.sky@foxmail.com>
2020-08-16 21:00:09 +0800

    add register service instance unit test

--- config/config_loader.go
+++ config/config_loader.go
@@ -27,6 +27,7 @@ import (
 )
 
 import (
+	gxnet "github.com/dubbogo/gost/net"
 	perrors "github.com/pkg/errors"
 )
 
@@ -212,7 +213,7 @@ func loadProviderConfig() {
 }
 
 // registerServiceInstance register service instance
-func registerServiceInstance(){
+func registerServiceInstance() {
 	url := selectMetadataServiceExportedURL()
 	if url == nil {
 		return
