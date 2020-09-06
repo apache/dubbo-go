894820f71ded12af7a5bd1e501396178bf905a44
fangyincheng <fangyincheng@sina.com>
2020-09-07 02:02:09 +0800

    fix comments

--- protocol/dubbo/dubbo_codec.go
+++ protocol/dubbo/dubbo_codec.go
@@ -18,7 +18,6 @@ package dubbo
 
 import (
 	"bytes"
-	"github.com/apache/dubbo-go/protocol/dubbo/impl"
 	"strconv"
 	"time"
 )
@@ -32,6 +31,7 @@ import (
 	"github.com/apache/dubbo-go/common/constant"
 	"github.com/apache/dubbo-go/common/logger"
 	"github.com/apache/dubbo-go/protocol"
+	"github.com/apache/dubbo-go/protocol/dubbo/impl"
 	"github.com/apache/dubbo-go/protocol/invocation"
 	"github.com/apache/dubbo-go/remoting"
 )
