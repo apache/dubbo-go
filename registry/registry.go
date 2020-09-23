94496c5a803713aaa45677f8e61ddf8cb62e4c2e
cvictory <shenglicao2@gmail.com>
2020-09-23 20:41:36 +0800

    fix review issue: add constraint of Action value of ServiceEvent in NotifyAll func

--- registry/registry.go
+++ registry/registry.go
@@ -73,8 +73,8 @@ type NotifyListener interface {
 	// passed in, then it's a incremental event. Pls. note when a list (instead of single event) comes,
 	// the impl of NotifyListener may abandon the accumulated result from previous notifications.
 	Notify(*ServiceEvent)
-	// Notify the events are complete Service Event List.
-	// The argument of events []*ServiceEvent is equal to urls []*URL, because Action of ServiceEvent will be ignored.
+	// NotifyAll the events are complete Service Event List.
+	// The argument of events []*ServiceEvent is equal to urls []*URL, The Action of serviceEvent should be EventTypeUpdate.
 	// If your registry center can only get all urls but can't get individual event, you should use this one.
 	NotifyAll([]*ServiceEvent)
 }
