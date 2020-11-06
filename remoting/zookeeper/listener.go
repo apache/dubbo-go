e9bf6a20a16a0ad55c8283b1be18414ae0cb875a
cadeeper <cadeeper@gmail.com>
2020-11-06 14:12:50 +0800

    fix bug: pathMap cache need to delete after node removed

--- remoting/zookeeper/listener.go
+++ remoting/zookeeper/listener.go
@@ -302,8 +302,8 @@ func (l *ZkEventListener) listenDirEvent(conf *common.URL, zkPath string, listen
 				if l.listenServiceNodeEvent(zkPath) {
 					listener.DataChange(remoting.Event{Path: zkPath, Action: remoting.EventTypeDel})
 					l.pathMapLock.Lock()
+					defer l.pathMapLock.Unlock()
 					delete(l.pathMap, zkPath)
-					l.pathMapLock.Unlock()
 				}
 				logger.Warnf("listenSelf(zk path{%s}) goroutine exit now", zkPath)
 			}(dubboPath, listener)
