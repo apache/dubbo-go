590226bd4cd9b06299bebd447b61ecae275bf0fa
AlexStocks <alexstocks@foxmail.com>
2021-01-03 02:50:33 +0800

    fix protocol linter error

--- protocol/dubbo/hessian2/hessian_request.go
+++ protocol/dubbo/hessian2/hessian_request.go
@@ -30,6 +30,10 @@ import (
 	perrors "github.com/pkg/errors"
 )
 
+import (
+	"github.com/apache/dubbo-go/common/logger"
+)
+
 /////////////////////////////////////////
 // dubbo
 /////////////////////////////////////////
@@ -221,10 +225,18 @@ func packRequest(service Service, header DubboHeader, req interface{}) ([]byte, 
 	}
 
 	// dubbo version + path + version + method
-	encoder.Encode(DEFAULT_DUBBO_PROTOCOL_VERSION)
-	encoder.Encode(service.Path)
-	encoder.Encode(service.Version)
-	encoder.Encode(service.Method)
+	if err := encoder.Encode(DEFAULT_DUBBO_PROTOCOL_VERSION); err != nil {
+		logger.Error("Encode(DEFAULT_DUBBO_PROTOCOL_VERSION) = error: %v", err)
+	}
+	if err := encoder.Encode(service.Path); err != nil {
+		logger.Error("Encode(service.Path) = error: %v", err)
+	}
+	if err := encoder.Encode(service.Version); err != nil {
+		logger.Error("Encode(service.Version) = error: %v", err)
+	}
+	if err := encoder.Encode(service.Method); err != nil {
+		logger.Error("Encode(service.Method) = error: %v", err)
+	}
 
 	// args = args type list + args value list
 	if types, err = getArgsTypeList(args); err != nil {
