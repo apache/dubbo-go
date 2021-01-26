8800175bed65e8d3fca84d86548a04f8d132878d
LaurenceLiZhixin <382673304@qq.com>
2021-01-26 19:27:14 +0800

    fix: add log to show blacklist changes

--- protocol/rpc_status.go
+++ protocol/rpc_status.go
@@ -210,12 +210,14 @@ func GetInvokerHealthyStatus(invoker Invoker) bool {
 // SetInvokerUnhealthyStatus add target invoker to black list
 func SetInvokerUnhealthyStatus(invoker Invoker) {
 	invokerBlackList.Store(invoker.GetUrl().Key(), invoker)
+	logger.Info("Add invoker ip = ", invoker.GetUrl().Location, " to black list")
 	blackListCacheDirty.Store(true)
 }
 
 // RemoveInvokerUnhealthyStatus remove unhealthy status of target invoker from blacklist
 func RemoveInvokerUnhealthyStatus(invoker Invoker) {
 	invokerBlackList.Delete(invoker.GetUrl().Key())
+	logger.Info("Remove invoker ip = ", invoker.GetUrl().Location, " from black list")
 	blackListCacheDirty.Store(true)
 }
 
@@ -235,6 +237,7 @@ func GetBlackListInvokers(blockSize int) []Invoker {
 // RemoveUrlKeyUnhealthyStatus called when event of provider unregister, delete from black list
 func RemoveUrlKeyUnhealthyStatus(key string) {
 	invokerBlackList.Delete(key)
+	logger.Info("Remove invoker key = ", key, " from black list")
 	blackListCacheDirty.Store(true)
 }
 
