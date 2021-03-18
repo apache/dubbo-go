e0789f412589c85ada49eb503d09829563fe6666
xiejiajun <xiejiajun02@163.con>
2021-03-19 00:37:47 +0800

    RpcClientPackageHandler error compare code improve

--- remoting/getty/readwriter.go
+++ remoting/getty/readwriter.go
@@ -53,8 +53,7 @@ func (p *RpcClientPackageHandler) Read(ss getty.Session, data []byte) (interface
 	resp, length, err := (p.client.codec).Decode(data)
 	//err := pkg.Unmarshal(buf, p.client)
 	if err != nil {
-		err = perrors.Cause(err)
-		if err == hessian.ErrHeaderNotEnough || err == hessian.ErrBodyNotEnough {
+		if errors.Is(err, hessian.ErrHeaderNotEnough) || errors.Is(err, hessian.ErrBodyNotEnough) {
 			return nil, 0, nil
 		}
 
