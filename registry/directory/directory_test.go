b9a8a1a94ee89f3ee97395222ba0550a7769ac59
LaurenceLiZhixin <382673304@qq.com>
2021-03-19 11:36:17 +0800

    fix: fix test

--- registry/directory/directory_test.go
+++ registry/directory/directory_test.go
@@ -87,7 +87,7 @@ func Test_List(t *testing.T) {
 	registryDirectory, _ := normalRegistryDir()
 
 	time.Sleep(6e9)
-	assert.Len(t, registryDirectory.List(&invocation.RPCInvocation{}), 10)
+	assert.Len(t, registryDirectory.List(&invocation.RPCInvocation{}), 3)
 	assert.Equal(t, true, registryDirectory.IsAvailable())
 
 }
