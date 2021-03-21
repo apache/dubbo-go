fb2fb83c135eba6146c725cce362790f1b89215e
LaurenceLiZhixin <382673304@qq.com>
2021-03-22 00:32:26 +0800

    fix cr

--- common/proxy/proxy_factory/pass_through_test.go
+++ common/proxy/proxy_factory/pass_through_test.go
@@ -1,5 +1,3 @@
-package proxy_factory
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package proxy_factory
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package proxy_factory
 
 import (
 	"fmt"
