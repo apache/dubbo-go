a1e8e535d2c66a1076c17b840ada2a9d371cd29f
Patrick <dreamlike.sky@foxmail.com>
2021-04-19 16:32:37 +0800

    add BaseApplicationMetadataIdentifier

--- metadata/identifier/base_metadata_identifier.go
+++ metadata/identifier/base_metadata_identifier.go
@@ -90,3 +90,20 @@ func withPathSeparator(path string) string {
 	}
 	return path
 }
+
+// BaseApplicationMetadataIdentifier is the base implement of BaseApplicationMetadataIdentifier interface
+type BaseApplicationMetadataIdentifier struct {
+	Application string
+}
+
+// getIdentifierKey returns string that format is service:Version:Group:Side:param1:param2...
+func (madi *BaseApplicationMetadataIdentifier) getIdentifierKey(params ...string) string {
+	return madi.Application + joinParams(constant.KEY_SEPARATOR, params)
+}
+
+// getFilePathKey returns string that format is metadata/application/revision
+func (madi *BaseApplicationMetadataIdentifier) getFilePathKey(params ...string) string {
+	return constant.DEFAULT_PATH_TAG +
+		madi.Application +
+		joinParams(constant.PATH_SEPARATOR, params)
+}
--- metadata/identifier/subscribe_metadata_identifier.go
+++ metadata/identifier/subscribe_metadata_identifier.go
@@ -20,25 +20,24 @@ package identifier
 // SubscriberMetadataIdentifier is inherit baseMetaIdentifier with service params: Revision
 type SubscriberMetadataIdentifier struct {
 	Revision string
-	MetadataIdentifier
+	BaseApplicationMetadataIdentifier
 }
 
 func NewSubscriberMetadataIdentifier(application string, revision string) *SubscriberMetadataIdentifier {
 	return &SubscriberMetadataIdentifier{
 		Revision: revision,
-		MetadataIdentifier: MetadataIdentifier{
-			Application:            application,
-			BaseMetadataIdentifier: BaseMetadataIdentifier{},
+		BaseApplicationMetadataIdentifier: BaseApplicationMetadataIdentifier{
+			Application: application,
 		},
 	}
 }
 
 // GetIdentifierKey returns string that format is service:Version:Group:Side:Revision
 func (mdi *SubscriberMetadataIdentifier) GetIdentifierKey() string {
-	return mdi.BaseMetadataIdentifier.getIdentifierKey(mdi.Revision)
+	return mdi.BaseApplicationMetadataIdentifier.getIdentifierKey(mdi.Revision)
 }
 
 // GetFilePathKey returns string that format is metadata/path/Version/Group/Side/Revision
 func (mdi *SubscriberMetadataIdentifier) GetFilePathKey() string {
-	return mdi.BaseMetadataIdentifier.getFilePathKey(mdi.Revision)
+	return mdi.BaseApplicationMetadataIdentifier.getFilePathKey(mdi.Revision)
 }
