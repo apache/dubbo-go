213f7d5bc2ba8184e58f008f3d61b3c6c800cfce
LaurenceLiZhixin <382673304@qq.com>
2021-04-20 15:12:41 +0800

    fix: fix nacos test

--- metadata/report/nacos/report_test.go
+++ metadata/report/nacos/report_test.go
@@ -18,7 +18,6 @@
 package nacos
 
 import (
-	"encoding/json"
 	"net/http"
 	"strconv"
 	"testing"
@@ -63,25 +62,8 @@ func TestNacosMetadataReport_CRUD(t *testing.T) {
 	assert.Equal(t, 1, len(exportedUrls))
 	assert.Nil(t, err)
 
-	subMi := newSubscribeMetadataIdentifier()
-	urls := []string{serviceUrl.String()}
-	bytes, _ := json.Marshal(urls)
-	err = rpt.SaveSubscribedData(subMi, string(bytes))
-	assert.Nil(t, err)
-
-	subscribeUrl, err := rpt.GetSubscribedURLs(subMi)
-	assert.Equal(t, 1, len(subscribeUrl))
-	assert.Nil(t, err)
-
 	err = rpt.RemoveServiceMetadata(serviceMi)
 	assert.Nil(t, err)
-}
-
-func newSubscribeMetadataIdentifier() *identifier.SubscriberMetadataIdentifier {
-	return &identifier.SubscriberMetadataIdentifier{
-		Revision:                          "subscribe",
-		BaseApplicationMetadataIdentifier: identifier.BaseApplicationMetadataIdentifier{Application: "provider"},
-	}
 }
 
 func newServiceMetadataIdentifier() *identifier.ServiceMetadataIdentifier {
