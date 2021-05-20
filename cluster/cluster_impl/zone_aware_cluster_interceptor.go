22517f5719ab4192e1f26d70b1d1cf625ba96490
Ian Luo <ian.luo@gmail.com>
2021-05-20 18:31:12 +0800

    cluster interceptor support (#1206)
    * cluster interceptor support
    
    * fix go fmt issue
    
    * add license header
    
    * add license header
    
    * move interceptors into init method
    
    * make extension go-routine safe

--- cluster/cluster_impl/zone_aware_cluster_interceptor.go
+++ cluster/cluster_impl/zone_aware_cluster_interceptor.go
@@ -18,27 +18,40 @@
 package cluster_impl
 
 import (
+	"context"
+)
+
+import (
 	"github.com/apache/dubbo-go/cluster"
-	"github.com/apache/dubbo-go/common/extension"
+	"github.com/apache/dubbo-go/common/constant"
 	"github.com/apache/dubbo-go/protocol"
 )
 
-type availableCluster struct{}
-
-const available = "available"
-
-func init() {
-	extension.SetCluster(available, NewAvailableCluster)
+type zoneAwareInterceptor struct {
 }
 
-// NewAvailableCluster returns a cluster instance
-//
-// Obtain available service providers
-func NewAvailableCluster() cluster.Cluster {
-	return &availableCluster{}
+func (z *zoneAwareInterceptor) Invoke(ctx context.Context, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+	key := constant.REGISTRY_KEY + "." + constant.ZONE_FORCE_KEY
+	force := ctx.Value(key)
+
+	if force != nil {
+		switch value := force.(type) {
+		case bool:
+			if value {
+				invocation.SetAttachments(key, "true")
+			}
+		case string:
+			if "true" == value {
+				invocation.SetAttachments(key, "true")
+			}
+		default:
+			// ignore
+		}
+	}
+
+	return invoker.Invoke(ctx, invocation)
 }
 
-// Join returns a baseClusterInvoker instance
-func (cluster *availableCluster) Join(directory cluster.Directory) protocol.Invoker {
-	return buildInterceptorChain(NewAvailableClusterInvoker(directory))
+func getZoneAwareInterceptor() cluster.Interceptor {
+	return &zoneAwareInterceptor{}
 }
