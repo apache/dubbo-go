4dd3bf44d2238822bf415c333d24a0b61953478e
rai <rai>
2021-05-30 14:11:00 +0800

    Add: add default integration test

--- integrate_test.sh
+++ integrate_test.sh
@@ -25,20 +25,45 @@ echo 'start integrate-test'
 ROOT_DIR=$(pwd)
 echo "integrate-test root work-space -> ${ROOT_DIR}"
 
-# show all travis-env
-echo "travis current commit id  -> $2"
-echo "travis pull request branch -> ${GITHUB_REF}"
-echo "travis pull request slug -> ${GITHUB_REPOSITORY}"
-echo "travis pull request repo slug -> ${GITHUB_REPOSITORY}"
-echo "travis pull request actor -> ${GITHUB_ACTOR}"
-echo "travis pull request repo param -> $1"
+# show all github-env
+echo "github current commit id  -> $2"
+echo "github pull request branch -> ${GITHUB_REF}"
+echo "github pull request slug -> ${GITHUB_REPOSITORY}"
+echo "github pull request repo slug -> ${GITHUB_REPOSITORY}"
+echo "github pull request actor -> ${GITHUB_ACTOR}"
+echo "github pull request repo param -> $1"
+echo "github pull request base branch -> $3"
+echo "github pull request head branch -> ${GITHUB_HEAD_REF}"
 
-# clone dubbo-go-samples
-git clone -b 3.0 https://github.com/apache/dubbo-go-samples.git samples
-cd samples
+samples_testing() {
+    echo "use dubbo-go-samples $3 branch for integration testing"
+    git clone -b "$3" https://github.com/apache/dubbo-go-samples.git samples && cd samples
 
-# update dubbo-go to current commit id
-go mod edit -replace=dubbo.apache.org/dubbo-go/v3=github.com/"$1"/v3@"$2"
+    # update dubbo-go to current commit id
+    go mod edit -replace=dubbo.apache.org/dubbo-go/v3=github.com/"$1"/v3@"$2"
 
-# start integrate test
-./start_integrate_test.sh
+    # start integrate test
+    ./start_integrate_test.sh
+}
+
+local_testing() {
+    echo "use test/integrate/dubbo for integration testing"
+    # default use zk as registry
+    #start zookeeper registry insecure listen in [:]:2181
+    docker run -d --network host zookeeper
+    echo "zookeeper listen in [:]2181"
+
+    # build go-server image
+    cd ./test/integrate/dubbo/go-server
+    docker build . -t ci-provider --build-arg REPO="$1" --build-arg COMMITID="$2"
+    cd "${ROOT_DIR}"
+    docker run -d --network host ci-provider
+}
+
+# check dubbo-go-samples corresponding branch
+res=$(git ls-remote --heads https://github.com/apache/dubbo-go-samples.git "$3" | wc -l)
+if [ "$res" -eq "1" ]; then
+    samples_testing "$@"
+else
+    local_testing "$@"
+fi
