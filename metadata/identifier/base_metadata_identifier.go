c2e0e30751bced4d8ae811c08efb8ee077735e6a
william feng <williamfeng323@hotmail.com>
2021-07-01 11:14:34 +0800

    [#1288] fix the inconsistent filepath key logic

--- metadata/identifier/base_metadata_identifier.go
+++ metadata/identifier/base_metadata_identifier.go
@@ -18,7 +18,7 @@
 package identifier
 
 import (
-	"encoding/base64"
+	"net/url"
 )
 
 import (
@@ -68,21 +68,14 @@ func (mdi *BaseMetadataIdentifier) getFilePathKey(params ...string) string {
 		withPathSeparator(mdi.Group) +
 		withPathSeparator(mdi.Side) +
 		joinParams(constant.PATH_SEPARATOR, params)
-
 }
 
 // serviceToPath uss URL encode to decode the @serviceInterface
 func serviceToPath(serviceInterface string) string {
 	if serviceInterface == constant.ANY_VALUE {
 		return ""
-	} else {
-		decoded, err := base64.URLEncoding.DecodeString(serviceInterface)
-		if err != nil {
-			return ""
-		}
-		return string(decoded)
 	}
-
+	return url.PathEscape(serviceInterface)
 }
 
 // withPathSeparator return "/" + @path
