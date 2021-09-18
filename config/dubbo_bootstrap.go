22f9c7c8c46581ebff24e0fd28690ef3e07ec65d
zhaoyunxing <zhaoyunxing@apache.org>
2021-09-18 18:23:49 +0800

    代码结构调整

--- config/dubbo_bootstrap.go
+++ config/dubbo_bootstrap.go
@@ -18,11 +18,17 @@
 package config
 
 import (
+	"go.uber.org/atomic"
+)
+
+import (
+	hessian "github.com/apache/dubbo-go-hessian2"
+)
+
+import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/extension"
 	"dubbo.apache.org/dubbo-go/v3/common/logger"
-	hessian "github.com/apache/dubbo-go-hessian2"
-	"go.uber.org/atomic"
 )
 
 func GetInstance(opts ...RootConfigOpt) *RootConfig {
