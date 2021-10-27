6c354050039a32bd8b02cfc95290d8bf13382f0c
XavierNiu <a@nxw.name>
2021-10-27 11:57:54 +0800

    feat(cluster): add Metrics interface

--- cluster/metrics/local_metrics.go
+++ cluster/metrics/local_metrics.go
@@ -1,12 +1,24 @@
 package metrics
 
-import "dubbo.apache.org/dubbo-go/v3/protocol"
+import (
+	"dubbo.apache.org/dubbo-go/v3/common"
+	"fmt"
+)
 
 type LocalMetrics struct {
 	metrics map[string]interface{}
 }
 
+func (m *LocalMetrics) GetMethodMetrics(url *common.URL, methodName, key string) (interface{}, error) {
+	mkey := fmt.Sprintf("%s.%s.%s.%s", getInstanceKey(url), getInvokerKey(url), methodName, key)
+	panic(mkey)
+}
 
-func (m *LocalMetrics) GetMethodMetrics(ivk *protocol.Invoker, methodName, key string) (interface{}, error) {
 
+func (m *LocalMetrics) GetInvokerMetrics(url *common.URL, key string) (interface{}, error) {
+	panic("implement me")
+}
+
+func (m *LocalMetrics) GetInstanceMetrics(url *common.URL, key string) (interface{}, error) {
+	panic("implement me")
 }
