6603d64c8890c1c3321751bb063895121d8acddb
springliao <springliao@tencent.com>
2021-11-29 13:18:18 +0800

    fix: fix go fmt error

--- filter/auth/accesskey_storage.go
+++ filter/auth/accesskey_storage.go
@@ -19,20 +19,12 @@ package auth
 
 import (
 	"sync"
-	"sync"
-)
 
-import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
 	"dubbo.apache.org/dubbo-go/v3/common/extension"
 	"dubbo.apache.org/dubbo-go/v3/filter"
 	"dubbo.apache.org/dubbo-go/v3/protocol"
-)
-
-var (
-	storageOnce sync.Once
-	storage     *defaultAccesskeyStorage
 )
 
 var (
