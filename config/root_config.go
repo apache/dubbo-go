3ec923e4d6b4390a9cad74d62e21cfbb8e9438b4
rainshell <srs_1225@163.com>
2021-12-19 16:51:21 +0800

    imports format root_config.go and test

--- config/root_config.go
+++ config/root_config.go
@@ -23,19 +23,20 @@ import (
 )
 
 import (
-	hessian "github.com/apache/dubbo-go-hessian2"
-	"github.com/knadh/koanf"
-	perrors "github.com/pkg/errors"
-	"go.uber.org/atomic"
-)
-
-import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
 	"dubbo.apache.org/dubbo-go/v3/common/extension"
 	"dubbo.apache.org/dubbo-go/v3/common/logger"
 	"dubbo.apache.org/dubbo-go/v3/config_center"
 	"dubbo.apache.org/dubbo-go/v3/metadata/service/exporter"
+
+	hessian "github.com/apache/dubbo-go-hessian2"
+
+	"github.com/knadh/koanf"
+
+	perrors "github.com/pkg/errors"
+
+	"go.uber.org/atomic"
 )
 
 var (
