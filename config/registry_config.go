d6d59ae589a14b7f55c99b52b222b49f19ae1b77
chickenlj <ken.lj.hz@gmail.com>
2022-08-31 14:33:20 +0800

    fix log format

--- config/registry_config.go
+++ config/registry_config.go
@@ -178,8 +178,8 @@ func (c *RegistryConfig) toURLs(roleType common.RoleType) ([]*common.URL, error)
 	var registryURL *common.URL
 
 	if !isValid(c.Address) {
-		logger.Infof("Empty or N/A registry address found, the process will work with no registry enabled "+
-			"which means that the address of this instance will not be registered and not able to be found by other consumer instances.", c.Address)
+		logger.Infof("Empty or N/A registry address found, the process will work with no registry enabled " +
+			"which means that the address of this instance will not be registered and not able to be found by other consumer instances.")
 		return urls, nil
 	}
 
