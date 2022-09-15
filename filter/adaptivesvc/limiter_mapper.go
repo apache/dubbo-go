a0503c6e93b3bac1a5c0ac49ad636185cfb53e86
cooli <y.p.zhang8664@gmail.com>
2022-09-15 22:14:22 +0800

    fix: deadlock caused by rwMutex in limiter mapper

--- filter/adaptivesvc/limiter_mapper.go
+++ filter/adaptivesvc/limiter_mapper.go
@@ -58,6 +58,8 @@ func (m *limiterMapper) newAndSetMethodLimiter(url *common.URL, methodName strin
 		ok bool
 	)
 	m.rwMutex.Lock()
+	defer m.rwMutex.Unlock()
+
 	if l, ok = limiterMapperSingleton.mapper[key]; ok {
 		return l, nil
 	}
@@ -68,7 +70,6 @@ func (m *limiterMapper) newAndSetMethodLimiter(url *common.URL, methodName strin
 		return nil, ErrLimiterTypeNotFound
 	}
 	limiterMapperSingleton.mapper[key] = l
-	m.rwMutex.Unlock()
 	return l, nil
 }
 
