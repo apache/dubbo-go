1d647298ad95d768cc3fa2d1258c3dd5757fd2b7
chuntaojun <liaochuntao@live.com>
2022-11-19 08:43:40 +0800

    feat:support polaris tps limiter

--- imports/imports.go
+++ imports/imports.go
@@ -46,6 +46,7 @@ import (
 	_ "dubbo.apache.org/dubbo-go/v3/filter/hystrix"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/metrics"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/otel/trace"
+	_ "dubbo.apache.org/dubbo-go/v3/filter/polaris/limit"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/seata"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/sentinel"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/token"
