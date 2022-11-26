16f5a0f915170705e1b6512566c3dd0839c02079
liaochuntao <liaochuntao@live.com>
2022-11-26 20:57:52 +0800

    [ISSUE #2124] feat:support polaris router ability (#2132)
    * feat:support polaris router ability
    
    * feat:support polaris router ability
    
    * chore:update polaris-go version to 1.3.0-alpha
    
    * style:fix code style
    
    * style:fix code style
    
    * style:fix code style

--- imports/imports.go
+++ imports/imports.go
@@ -33,6 +33,7 @@ import (
 	_ "dubbo.apache.org/dubbo-go/v3/cluster/loadbalance/random"
 	_ "dubbo.apache.org/dubbo-go/v3/cluster/loadbalance/roundrobin"
 	_ "dubbo.apache.org/dubbo-go/v3/cluster/router/meshrouter"
+	_ "dubbo.apache.org/dubbo-go/v3/cluster/router/polaris"
 	_ "dubbo.apache.org/dubbo-go/v3/config_center/nacos"
 	_ "dubbo.apache.org/dubbo-go/v3/config_center/zookeeper"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/accesslog"
