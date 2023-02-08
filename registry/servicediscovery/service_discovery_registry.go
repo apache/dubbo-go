d67865a68bd59f6e02c46ad0b0d81f9909e799ce
bobtthp <bobtthp@bob1.local>
2023-02-08 17:14:28 +0800

    rollback loadbalance change

--- registry/servicediscovery/service_discovery_registry.go
+++ registry/servicediscovery/service_discovery_registry.go
@@ -42,6 +42,7 @@ import (
 	"dubbo.apache.org/dubbo-go/v3/registry"
 	"dubbo.apache.org/dubbo-go/v3/registry/event"
 	"dubbo.apache.org/dubbo-go/v3/registry/servicediscovery/synthesizer"
+	"dubbo.apache.org/dubbo-go/v3/remoting"
 )
 
 func init() {
@@ -244,7 +245,32 @@ func (s *ServiceDiscoveryRegistry) SubscribeURL(url *common.URL, notify registry
 }
 
 // LoadSubscribeInstances load subscribe instance
-func (s *ServiceDiscoveryRegistry) LoadSubscribeInstances(_ *common.URL, _ registry.NotifyListener) error {
+func (s *ServiceDiscoveryRegistry) LoadSubscribeInstances(url *common.URL, notify registry.NotifyListener) error {
+	appName := url.GetParam(constant.ApplicationKey, url.Username)
+	instances := s.serviceDiscovery.GetInstances(appName)
+	for _, instance := range instances {
+		if instance.GetMetadata() == nil {
+			logger.Warnf("Instance metadata is nil: %s", instance.GetHost())
+			continue
+		}
+		revision, ok := instance.GetMetadata()[constant.ExportedServicesRevisionPropertyName]
+		if !ok {
+			logger.Warnf("Instance metadata revision is nil: %s", instance.GetHost())
+			continue
+		}
+		if "0" == revision {
+			logger.Infof("Find instance without valid service metadata: %s", instance.GetHost())
+			continue
+		}
+		metadataInfo, err := event.GetMetadataInfo(instance, revision)
+		if err != nil {
+			return err
+		}
+		instance.SetServiceMetadata(metadataInfo)
+		for _, url := range instance.ToURLs() {
+			notify.Notify(&registry.ServiceEvent{Action: remoting.EventTypeAdd, Service: url})
+		}
+	}
 	return nil
 }
 
