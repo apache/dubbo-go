da5ddd4e86071d4298d319788c62ac57e497a17a
FinalT <finalt1361@163.com>
2023-04-11 14:23:19 +0800

    Implement condition routing basic functions and complete related tests

---
+++ cluster/router/condition/route.go
@@ -0,0 +1,279 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package condition
+
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+import (
+	"github.com/dubbogo/gost/log/logger"
+
+	"github.com/pkg/errors"
+
+	"gopkg.in/yaml.v2"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/cluster/router/condition/matcher"
+	"dubbo.apache.org/dubbo-go/v3/common"
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/common/extension"
+	"dubbo.apache.org/dubbo-go/v3/config"
+	"dubbo.apache.org/dubbo-go/v3/config_center"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+)
+
+var (
+	RoutePattern = regexp.MustCompile("([&!=,]*)\\s*([^&!=,\\s]+)")
+)
+
+type ConditionStateRouter struct {
+	enable           bool
+	force            bool
+	url              *common.URL
+	whenCondition    map[string]matcher.ConditionMatcher
+	thenCondition    map[string]matcher.ConditionMatcher
+	matcherFactories []matcher.ConditionMatcherFactory
+}
+
+func NewConditionStateRouter(url *common.URL) (*ConditionStateRouter, error) {
+
+	matcherFactories := extension.GetMatcherFactories()
+	if len(matcherFactories) == 0 {
+		return nil, errors.Errorf("No ConditionMatcherFactory exists")
+	}
+	factories := make([]matcher.ConditionMatcherFactory, 0, len(matcherFactories))
+	for _, matcherFactory := range matcherFactories {
+		factories = append(factories, matcherFactory())
+	}
+	sortMatcherFactories(factories)
+
+	force := url.GetParamBool(constant.ForceKey, false)
+	enable := url.GetParamBool(constant.EnabledKey, true)
+	c := &ConditionStateRouter{
+		url:              url,
+		force:            force,
+		matcherFactories: factories,
+		enable:           enable,
+	}
+
+	if enable {
+		rule := url.GetParam(constant.RuleKey, "")
+		if rule == "" || len(strings.Trim(rule, " ")) == 0 {
+			return nil, errors.Errorf("Illegal route rule!")
+		}
+		rule = strings.Replace(rule, "consumer.", "", -1)
+		rule = strings.Replace(rule, "provider.", "", -1)
+		i := strings.Index(rule, "=>")
+		// for the case of `{when rule} => {then rule}`
+		var whenRule string
+		var thenRule string
+		if i < 0 {
+			whenRule = ""
+			thenRule = strings.Trim(rule, " ")
+		} else {
+			whenRule = strings.Trim(rule[0:i], " ")
+			thenRule = strings.Trim(rule[i+2:], " ")
+		}
+		var when map[string]matcher.ConditionMatcher
+		var then map[string]matcher.ConditionMatcher
+		var err error
+		if whenRule == "" || whenRule == " " || whenRule == "true" {
+			when = make(map[string]matcher.ConditionMatcher)
+		} else {
+			when, err = c.parseRule(whenRule)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if thenRule == "" || thenRule == " " || thenRule == "false" {
+			then = nil
+		} else {
+			then, err = c.parseRule(thenRule)
+			if err != nil {
+				return nil, err
+			}
+		}
+		// NOTE: It should be determined on the business level whether the `When condition` can be empty or not.
+		c.whenCondition = when
+		c.thenCondition = then
+	}
+	return c, nil
+}
+
+func (c *ConditionStateRouter) Route(invokers []protocol.Invoker, url *common.URL, invocation protocol.Invocation) []protocol.Invoker {
+	if !c.enable {
+		return invokers
+	}
+
+	if len(invokers) == 0 {
+		return invokers
+	}
+
+	if !c.matchWhen(url, invocation) {
+		return invokers
+	}
+
+	if len(c.thenCondition) == 0 {
+		logger.Warn("condition state router thenCondition is empty")
+		return []protocol.Invoker{}
+	}
+
+	var result = make([]protocol.Invoker, 0, len(invokers))
+	for _, invoker := range invokers {
+		if c.matchThen(invoker.GetURL(), url) {
+			result = append(result, invoker)
+		}
+	}
+
+	if len(result) != 0 {
+		return result
+	} else if c.force {
+		logger.Warn("execute condition state router result list is empty. and force=true")
+		return result
+	}
+
+	return invokers
+}
+
+func (c *ConditionStateRouter) URL() *common.URL {
+	return c.url
+}
+
+func (c *ConditionStateRouter) Priority() int64 {
+	return 0
+}
+
+func (c *ConditionStateRouter) Notify(invokers []protocol.Invoker) {
+}
+
+func (c *ConditionStateRouter) Process(event *config_center.ConfigChangeEvent) {
+}
+
+func (c *ConditionStateRouter) parseRule(rule string) (map[string]matcher.ConditionMatcher, error) {
+	condition := make(map[string]matcher.ConditionMatcher)
+	if rule == "" || rule == " " {
+		return condition, nil
+	}
+	// Key-Value pair, stores both match and mismatch conditions
+	var matcherPair matcher.ConditionMatcher
+	// Multiple values
+	values := make(map[string]struct{})
+	allMatchers := RoutePattern.FindAllStringSubmatch(rule, -1)
+	for _, matchers := range allMatchers {
+		separator := matchers[1]
+		content := matchers[2]
+		// Start part of the condition expression.
+		if separator == "" {
+			matcherPair = c.getMatcher(content)
+			condition[content] = matcherPair
+		} else if "&" == separator {
+			// The KV part of the condition expression
+			if condition[content] == nil {
+				matcherPair = c.getMatcher(content)
+				condition[content] = matcherPair
+			} else {
+				matcherPair = condition[content]
+			}
+		} else if "=" == separator {
+			// The Value in the KV part.
+			if matcherPair == nil {
+				return nil, errors.Errorf("Illegal route rule \"%s\", The error char '%s' before '%s'",
+					rule, separator, content)
+			}
+			values = matcherPair.GetMatches()
+			values[content] = struct{}{}
+		} else if "!=" == separator {
+			// The Value in the KV part.
+			if matcherPair == nil {
+				return nil, errors.Errorf("Illegal route rule \"%s\", The error char '%s' before '%s'",
+					rule, separator, content)
+			}
+			values = matcherPair.GetMismatches()
+			values[content] = struct{}{}
+		} else if "," == separator { // Should be separated by ','
+			// The Value in the KV part, if Value have more than one items.
+			if values == nil || len(values) == 0 {
+				return nil, errors.Errorf("Illegal route rule \"%s\", The error char '%s' before '%s'",
+					rule, separator, content)
+			}
+			values[content] = struct{}{}
+		} else {
+			return nil, errors.Errorf("Illegal route rule \"%s\", The error char '%s' before '%s'",
+				rule, separator, content)
+		}
+	}
+	return condition, nil
+}
+
+func (c *ConditionStateRouter) getMatcher(key string) matcher.ConditionMatcher {
+	for _, factory := range c.matcherFactories {
+		if factory.ShouldMatch(key) {
+			return factory.NewMatcher(key)
+		}
+	}
+	return extension.GetMatcherFactory("param").NewMatcher(key)
+}
+
+func (c *ConditionStateRouter) matchWhen(url *common.URL, invocation protocol.Invocation) bool {
+	if len(c.whenCondition) == 0 {
+		return true
+	}
+	return doMatch(url, nil, invocation, c.whenCondition, true)
+}
+
+func (c *ConditionStateRouter) matchThen(url *common.URL, param *common.URL) bool {
+	if len(c.thenCondition) == 0 {
+		return false
+	}
+	return doMatch(url, param, nil, c.thenCondition, false)
+}
+
+func doMatch(url *common.URL, param *common.URL, invocation protocol.Invocation, conditions map[string]matcher.ConditionMatcher, isWhenCondition bool) bool {
+	sample := url.ToMap()
+	for _, matcherPair := range conditions {
+		if !matcherPair.IsMatch(sample, param, invocation, isWhenCondition) {
+			return false
+		}
+	}
+	return true
+}
+
+func sortMatcherFactories(matcherFactories []matcher.ConditionMatcherFactory) {
+	sort.Stable(byPriority(matcherFactories))
+}
+
+type byPriority []matcher.ConditionMatcherFactory
+
+func (a byPriority) Len() int           { return len(a) }
+func (a byPriority) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byPriority) Less(i, j int) bool { return a[i].Priority() < a[j].Priority() }
+
+func parseRoute(routeContent string) (*config.RouterConfig, error) {
+	routeDecoder := yaml.NewDecoder(strings.NewReader(routeContent))
+	routerConfig := &config.RouterConfig{}
+	err := routeDecoder.Decode(routerConfig)
+	if err != nil {
+		return nil, err
+	}
+	return routerConfig, nil
+}
---
+++ cluster/router/condition/router_test.go
@@ -0,0 +1,402 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package condition
+
+import (
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/common"
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+	"dubbo.apache.org/dubbo-go/v3/protocol/invocation"
+)
+
+const (
+	LocalHost = "127.0.0.1"
+)
+
+func TestRouteMatchWhen(t *testing.T) {
+
+	rpcInvocation := invocation.NewRPCInvocation("getFoo", nil, nil)
+	whenConsumerURL, _ := common.NewURL("consumer://1.1.1.1/com.foo.BarService")
+
+	testData := []struct {
+		name        string
+		consumerUrl *common.URL
+		rule        string
+
+		wantVal bool
+	}{
+		{
+			name:        " => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        " => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+		{
+			name:        "host = 1.2.3.4 => ",
+			consumerUrl: whenConsumerURL,
+			rule:        "host = 1.2.3.4 => ",
+
+			wantVal: false,
+		},
+		{
+			name:        "host = 2.2.2.2,1.1.1.1,3.3.3.3 => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        "host = 2.2.2.2,1.1.1.1,3.3.3.3 => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+		{
+			name:        "host = 2.2.2.2,1.1.1.1,3.3.3.3 & host !=1.1.1.1 => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        "host = 2.2.2.2,1.1.1.1,3.3.3.3 & host !=1.1.1.1 => host = 1.2.3.4",
+
+			wantVal: false,
+		},
+		{
+			name:        "host !=4.4.4.4 & host = 2.2.2.2,1.1.1.1,3.3.3.3 => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        "host !=4.4.4.4 & host = 2.2.2.2,1.1.1.1,3.3.3.3 => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+		{
+			name:        "host !=4.4.4.* & host = 2.2.2.2,1.1.1.1,3.3.3.3 => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        "host !=4.4.4.* & host = 2.2.2.2,1.1.1.1,3.3.3.3 => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+		{
+			name:        "host = 2.2.2.2,1.1.1.*,3.3.3.3 & host != 1.1.1.1 => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        "host = 2.2.2.2,1.1.1.*,3.3.3.3 & host != 1.1.1.1 => host = 1.2.3.4",
+
+			wantVal: false,
+		},
+		{
+			name:        "host = 2.2.2.2,1.1.1.*,3.3.3.3 & host != 1.1.1.2 => host = 1.2.3.4",
+			consumerUrl: whenConsumerURL,
+			rule:        "host = 2.2.2.2,1.1.1.*,3.3.3.3 & host != 1.1.1.2 => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.name, func(t *testing.T) {
+			url, err := common.NewURL("condition://0.0.0.0/com.foo.BarService")
+			assert.Nil(t, err)
+			url.AddParam(constant.RuleKey, data.rule)
+			router, err := NewConditionStateRouter(url)
+			assert.Nil(t, err)
+			resVal := router.matchWhen(data.consumerUrl, rpcInvocation)
+			assert.Equal(t, data.wantVal, resVal)
+		})
+	}
+}
+
+// TestRouteMatchFilter also tests wildcard.WildcardValuePattern's Match method
+func TestRouteMatchFilter(t *testing.T) {
+
+	consumerURL, _ := common.NewURL("consumer://" + LocalHost + "/com.foo.BarService")
+	url1, _ := common.NewURL("dubbo://10.20.3.3:20880/com.foo.BarService?serialization=fastjson")
+	url2, _ := common.NewURL("dubbo://" + LocalHost + ":20880/com.foo.BarService")
+	url3, _ := common.NewURL("dubbo://" + LocalHost + ":20880/com.foo.BarService")
+
+	rpcInvocation := invocation.NewRPCInvocation("getFoo", nil, nil)
+
+	ink1 := protocol.NewBaseInvoker(url1)
+	ink2 := protocol.NewBaseInvoker(url2)
+	ink3 := protocol.NewBaseInvoker(url3)
+
+	invokerList := make([]protocol.Invoker, 0, 3)
+	invokerList = append(invokerList, ink1)
+	invokerList = append(invokerList, ink2)
+	invokerList = append(invokerList, ink3)
+
+	testData := []struct {
+		name        string
+		comsumerURL *common.URL
+		rule        string
+
+		wantVal int
+	}{
+		{
+			name:        "host = " + LocalHost + " => " + " host = 10.20.3.3",
+			comsumerURL: consumerURL,
+			rule:        "host = " + LocalHost + " => " + " host = 10.20.3.3",
+
+			wantVal: 1,
+		},
+		{
+			name:        "host = " + LocalHost + " => " + " host = 10.20.3.* & host != 10.20.3.3",
+			comsumerURL: consumerURL,
+			rule:        "host = " + LocalHost + " => " + " host = 10.20.3.* & host != 10.20.3.3",
+
+			wantVal: 0,
+		},
+		{
+			name:        "host = " + LocalHost + " => " + " host = 10.20.3.3  & host != 10.20.3.3",
+			comsumerURL: consumerURL,
+			rule:        "host = " + LocalHost + " => " + " host = 10.20.3.3  & host != 10.20.3.3",
+
+			wantVal: 0,
+		},
+		{
+			name:        "host = " + LocalHost + " => " + " host = 10.20.3.2,10.20.3.3,10.20.3.4",
+			comsumerURL: consumerURL,
+			rule:        "host = " + LocalHost + " => " + " host = 10.20.3.2,10.20.3.3,10.20.3.4",
+
+			wantVal: 1,
+		},
+		{
+			name:        "host = " + LocalHost + " => " + " host != 10.20.3.3",
+			comsumerURL: consumerURL,
+			rule:        "host = " + LocalHost + " => " + " host != 10.20.3.3",
+
+			wantVal: 2,
+		},
+		{
+			name:        "host = " + LocalHost + " => " + " serialization = fastjson",
+			comsumerURL: consumerURL,
+			rule:        "host = " + LocalHost + " => " + " serialization = fastjson",
+
+			wantVal: 1,
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.name, func(t *testing.T) {
+			url, err := common.NewURL("condition://" + LocalHost + "/com.foo.BarService")
+			assert.Nil(t, err)
+			url.AddParam(constant.RuleKey, data.rule)
+			url.AddParam(constant.ForceKey, "true")
+
+			router, err := NewConditionStateRouter(url)
+			assert.Nil(t, err)
+
+			filteredInvokers := router.Route(invokerList, data.comsumerURL, rpcInvocation)
+			resVal := len(filteredInvokers)
+			assert.Equal(t, data.wantVal, resVal)
+		})
+	}
+}
+
+func TestRouterMethodRoute(t *testing.T) {
+
+	rpcInvocation := invocation.NewRPCInvocation("getFoo", nil, nil)
+
+	testData := []struct {
+		name        string
+		consumerURL string
+		rule        string
+
+		wantVal bool
+	}{
+		{
+			name:        "More than one methods, mismatch",
+			consumerURL: "consumer://1.1.1.1/com.foo.BarService?methods=setFoo,getFoo,findFoo",
+			rule:        "methods=getFoo => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+		{
+			name:        "Exactly one method, match",
+			consumerURL: "consumer://1.1.1.1/com.foo.BarService?methods=getFoo",
+			rule:        "methods=getFoo => host = 1.2.3.4",
+
+			wantVal: true,
+		},
+		{
+			name:        "Method routing and Other condition routing can work together",
+			consumerURL: "consumer://1.1.1.1/com.foo.BarService?methods=getFoo",
+			rule:        "methods=getFoo & host!=1.1.1.1 => host = 1.2.3.4",
+
+			wantVal: false,
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.name, func(t *testing.T) {
+			url, err := common.NewURL("condition://0.0.0.0/com.foo.BarService")
+			assert.Nil(t, err)
+			url.AddParam(constant.RuleKey, data.rule)
+			router, err := NewConditionStateRouter(url)
+			assert.Nil(t, err)
+			consumer, _ := common.NewURL(data.consumerURL)
+			resVal := router.matchWhen(consumer, rpcInvocation)
+			assert.Equal(t, data.wantVal, resVal)
+		})
+	}
+}
+
+func TestRouteReturn(t *testing.T) {
+
+	rpcInvocation := invocation.NewRPCInvocation("getFoo", nil, nil)
+	consumerURL, _ := common.NewURL("consumer://" + LocalHost + "/com.foo.BarService")
+
+	testData := []struct {
+		name string
+		urls []string
+		rule string
+
+		wantUrls []string
+		wantVal  int
+	}{
+		{
+			name: "ReturnFalse",
+			urls: []string{
+				"",
+				"",
+				"",
+			},
+			rule: "host = " + LocalHost + " => false",
+
+			wantUrls: []string{},
+			wantVal:  0,
+		},
+		{
+			name: "ReturnEmpty",
+			urls: []string{
+				"",
+				"",
+				"",
+			},
+			rule: "host = " + LocalHost + " => ",
+
+			wantUrls: []string{},
+			wantVal:  0,
+		},
+		{
+			name: "ReturnAll",
+			urls: []string{
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			rule: "host = " + LocalHost + " => " + " host = " + LocalHost,
+
+			wantUrls: []string{
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			wantVal: 3,
+		},
+		{
+			name: "HostFilter",
+			urls: []string{
+				"dubbo://10.20.3.3:20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			rule: "host = " + LocalHost + " => " + " host = " + LocalHost,
+
+			wantUrls: []string{
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			wantVal: 2,
+		},
+		{
+			name: "EmptyHostFilter",
+			urls: []string{
+				"dubbo://10.20.3.3:20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			rule: " => " + " host = " + LocalHost,
+
+			wantUrls: []string{
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			wantVal: 2,
+		},
+		{
+			name: "FalseHostFilter",
+			urls: []string{
+				"dubbo://10.20.3.3:20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			rule: "true => " + " host = " + LocalHost,
+
+			wantUrls: []string{
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			wantVal: 2,
+		},
+		{
+			name: "PlaceHolder",
+			urls: []string{
+				"dubbo://10.20.3.3:20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			rule: "host = " + LocalHost + " => " + " host = $host",
+
+			wantUrls: []string{
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+				"dubbo://" + LocalHost + ":20880/com.foo.BarService",
+			},
+			wantVal: 2,
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.name, func(t *testing.T) {
+
+			invokers := make([]protocol.Invoker, 0, len(data.urls))
+			for _, urlStr := range data.urls {
+				url, _ := common.NewURL(urlStr)
+				invoker := protocol.NewBaseInvoker(url)
+				invokers = append(invokers, invoker)
+			}
+
+			wantInvokers := make([]protocol.Invoker, 0, len(data.wantUrls))
+			for _, wantUrlStr := range data.wantUrls {
+				url, _ := common.NewURL(wantUrlStr)
+				invoker := protocol.NewBaseInvoker(url)
+				wantInvokers = append(wantInvokers, invoker)
+			}
+
+			url, err := common.NewURL("condition://" + LocalHost + "/com.foo.BarService")
+			assert.Nil(t, err)
+			url.AddParam(constant.RuleKey, data.rule)
+			router, err := NewConditionStateRouter(url)
+			assert.Nil(t, err)
+
+			filterInvokers := router.Route(invokers, consumerURL, rpcInvocation)
+			resVal := len(filterInvokers)
+
+			assert.Equal(t, data.wantVal, resVal)
+			assert.Equal(t, wantInvokers, filterInvokers)
+		})
+	}
+}
