dee19c34ca209191d24910c4ce5a1c9c43834ea7
WangGuan <wg1024code@gmail.com>
2023-04-26 18:20:29 +0800

    refactor: split metrics module into multiple files

--- metrics/reporter.go
+++ metrics/reporter.go
@@ -20,9 +20,7 @@ package metrics
 import (
 	"context"
 	"time"
-)
 
-import (
 	"dubbo.apache.org/dubbo-go/v3/protocol"
 )
 
@@ -61,6 +59,6 @@ func NewReporterConfig() *ReporterConfig {
 //
 // Report method reports the duration of an invocation.
 type Reporter interface {
-	Report(ctx context.Context, invoker protocol.Invoker, invocation protocol.Invocation,
+	ReportAfterInvocation(ctx context.Context, invoker protocol.Invoker, invocation protocol.Invocation,
 		cost time.Duration, res protocol.Result)
 }
