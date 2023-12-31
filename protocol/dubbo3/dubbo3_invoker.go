27e22611bfa59ed59a62ffbf6c8911ad7ebf81b3
Qi, Xiaoqiang <qixiaoqiang@giantdc.com>
2023-12-31 12:18:51 +0800

    回滚对dubbo3_invoker.go中getTimeout的修改

--- protocol/dubbo3/dubbo3_invoker.go
+++ protocol/dubbo3/dubbo3_invoker.go
@@ -239,12 +239,12 @@ func (di *DubboInvoker) getTimeout(invocation *invocation_impl.RPCInvocation) ti
 	if len(timeout) != 0 {
 		if t, err := time.ParseDuration(timeout); err == nil {
 			// config timeout into attachment
-			invocation.SetAttachment(constant.TimeoutKey, strconv.Itoa(int(t.Nanoseconds())))
+			invocation.SetAttachment(constant.TimeoutKey, strconv.Itoa(int(t.Milliseconds())))
 			return t
 		}
 	}
 	// set timeout into invocation at method level
-	invocation.SetAttachment(constant.TimeoutKey, strconv.Itoa(int(di.timeout.Nanoseconds())))
+	invocation.SetAttachment(constant.TimeoutKey, strconv.Itoa(int(di.timeout.Milliseconds())))
 	return di.timeout
 }
 
