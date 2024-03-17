3657bef18d817e8464f471d2e6e147dea86a9d9f
jun <2456868764@qq.com>
2024-03-17 10:44:02 +0800

    init mtls,jwt,authn,rbac filter

---
+++ filter/mtls/filter.go
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package echo providers health check filter.
+// RPCService need a Echo method in consumer, if you want to use Filter
+// eg: Echo func(ctx context.Context, arg interface{}, rsp *Xxx) error
+package echo
+
+import (
+	"context"
+	"github.com/dubbogo/gost/log/logger"
+	"sync"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/common/extension"
+	"dubbo.apache.org/dubbo-go/v3/filter"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+)
+
+var (
+	once sync.Once
+	mtls *mtlsFilter
+)
+
+func init() {
+	extension.SetFilter(constant.MtlsFilterKey, newMtlsFilter)
+}
+
+type mtlsFilter struct{}
+
+func newMtlsFilter() filter.Filter {
+	if mtls == nil {
+		once.Do(func() {
+			mtls = &mtlsFilter{}
+		})
+	}
+	return mtls
+}
+
+// Invoke response to the callers with its first argument.
+func (f *mtlsFilter) Invoke(ctx context.Context, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+	if invocation.MethodName() == constant.Echo && len(invocation.Arguments()) == 1 {
+		return &protocol.RPCResult{
+			Rest:  invocation.Arguments()[0],
+			Attrs: invocation.Attachments(),
+		}
+	}
+	// TODO it just now echo logger, and implement mtls support check later
+	logger.Infof("mtls filter invoker:%v invocation:%v", invoker, invocation)
+	return invoker.Invoke(ctx, invocation)
+}
+
+// OnResponse dummy process, returns the result directly
+func (f *mtlsFilter) OnResponse(_ context.Context, result protocol.Result, _ protocol.Invoker,
+	_ protocol.Invocation) protocol.Result {
+
+	return result
+}
---
+++ filter/rbac/filter.go
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package echo providers health check filter.
+// RPCService need a Echo method in consumer, if you want to use Filter
+// eg: Echo func(ctx context.Context, arg interface{}, rsp *Xxx) error
+package echo
+
+import (
+	"context"
+	"github.com/dubbogo/gost/log/logger"
+	"sync"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/common/extension"
+	"dubbo.apache.org/dubbo-go/v3/filter"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+)
+
+var (
+	once sync.Once
+	rbac *rbacFilter
+)
+
+func init() {
+	extension.SetFilter(constant.RbacFilterKey, newRbacFilter)
+}
+
+type rbacFilter struct{}
+
+func newRbacFilter() filter.Filter {
+	if rbac == nil {
+		once.Do(func() {
+			rbac = &rbacFilter{}
+		})
+	}
+	return rbac
+}
+
+// Invoke response to the callers with its first argument.
+func (f *rbacFilter) Invoke(ctx context.Context, invoker protocol.Invoker, invocation protocol.Invocation) protocol.Result {
+	if invocation.MethodName() == constant.Echo && len(invocation.Arguments()) == 1 {
+		return &protocol.RPCResult{
+			Rest:  invocation.Arguments()[0],
+			Attrs: invocation.Attachments(),
+		}
+	}
+	// TODO it just now echo logger, and implement mtls support check later
+	logger.Infof("rbac filter invoker:%v invocation:%v", invoker, invocation)
+	return invoker.Invoke(ctx, invocation)
+}
+
+// OnResponse dummy process, returns the result directly
+func (f *rbacFilter) OnResponse(_ context.Context, result protocol.Result, _ protocol.Invoker,
+	_ protocol.Invocation) protocol.Result {
+
+	return result
+}
