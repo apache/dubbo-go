47f7e693753c1a4475a25927346f50005151a078
jun <2456868764@qq.com>
2024-04-04 22:09:02 +0800

    fix jwt authn unit test

--- istio/engine/jwt_authn_test.go
+++ istio/engine/jwt_authn_test.go
@@ -137,10 +137,9 @@ func TestJwtAuthnFilterEngine_Filter(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := &JwtAuthnFilterEngine{
-				headers:        tt.headers,
 				authentication: tt.authentication,
 			}
-			result, err := e.Filter()
+			result, err := e.Filter(tt.headers)
 			result.JwtToken = nil
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Filter() error = %v, wantErr %v", err, tt.wantErr)
