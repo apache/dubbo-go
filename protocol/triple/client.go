2d44df3c1c1ae4bbc57b85568e38c7fd19f8349f
xinfan.wu(吴歆帆) <xinfan.wu@xgimi.com>
2024-11-22 11:33:48 +0800

    chore:update comments for clarity

--- protocol/triple/client.go
+++ protocol/triple/client.go
@@ -131,7 +131,7 @@ func newClientManager(url *common.URL) (*clientManager, error) {
 		maxCallSendMsgSize = int(sendMsgSize)
 	}
 	cliOpts = append(cliOpts, tri.WithSendMaxBytes(maxCallSendMsgSize))
-	//set keepalive and keepalive timeout
+	//set keepalive inteval and keepalive timeout
 	keepAliveInterval := url.GetParamDuration(constant.KeepAliveInterval, "")
 	keepAliveTimeout := url.GetParamDuration(constant.KeepAliveTimeout, "")
 	var isIDL bool
