7a636cc42726bcba7d252c67b57cf318a6f8d6bd
zy张研 <yzhang55@trip.com>
2025-04-18 12:29:46 +0800

    feat: decode hessian from stream （#2840)

--- protocol/dubbo/hessian2/hessian_dubbo_test.go
+++ protocol/dubbo/hessian2/hessian_dubbo_test.go
@@ -245,3 +245,67 @@ type AttachTestObject struct {
 func (AttachTestObject) JavaClassName() string {
 	return "com.test.Test"
 }
+
+type CaseStream struct {
+	Payload string
+}
+
+func (CaseStream) JavaClassName() string {
+	return "com.test.CaseStream"
+}
+
+func TestDecodeFromTcpStream(t *testing.T) {
+	payload := make([]byte, 1024)
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	for i, _ := range payload {
+		payload[i] = alphabet[i%26]
+	}
+	cs := &CaseStream{
+		Payload: string(payload),
+	}
+
+	hessian.RegisterPOJO(cs)
+	codecW := NewHessianCodec(nil)
+	service := Service{
+		Path:      "test",
+		Interface: "ITest",
+		Version:   "v1.0",
+		Method:    "test",
+		Timeout:   time.Second * 10,
+	}
+	header := DubboHeader{
+		SerialID:       2,
+		Type:           PackageRequest,
+		ID:             1,
+		ResponseStatus: Zero,
+	}
+	resp, err := codecW.Write(service, header, []interface{}{cs})
+
+	// set reader buffer = 1024 to split resp into two parts
+	codec := NewStreamingHessianCodecCustom(0, bufio.NewReaderSize(bytes.NewReader(resp), 1024), 0)
+	h := &DubboHeader{}
+	assert.NoError(t, codec.ReadHeader(h))
+	assert.Equal(t, h.SerialID, header.SerialID)
+	assert.Equal(t, h.Type, header.Type)
+	assert.Equal(t, h.ID, header.ID)
+	assert.Equal(t, h.ResponseStatus, header.ResponseStatus)
+
+	reqBody := make([]interface{}, 7)
+
+	err = codec.ReadBody(reqBody)
+	assert.NoError(t, err)
+	assert.Equal(t, reqBody[1], service.Path)
+	assert.Equal(t, reqBody[2], service.Version)
+	assert.Equal(t, reqBody[3], service.Method)
+
+	if list, ok := reqBody[5].([]interface{}); ok {
+		assert.Len(t, list, 1)
+		if infoPtr, ok2 := list[0].(*CaseStream); ok2 {
+			assert.Equal(t, len(infoPtr.Payload), 1024)
+		}
+	}
+
+	codec = NewHessianCodecCustom(0, bufio.NewReaderSize(bytes.NewReader(resp), 1024), 0)
+	err = codec.ReadHeader(h)
+	assert.ErrorIs(t, err, ErrBodyNotEnough)
+}
