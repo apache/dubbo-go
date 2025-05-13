5da5f5965af5438030a0aebd0e871a8f96b79ec8
phpcyy <phpcyy@gmail.com>
2025-05-13 16:56:22 +0800

    fix: 修复无法设置 getty client 配置的问题

--- remoting/getty/getty_client.go
+++ remoting/getty/getty_client.go
@@ -48,14 +48,14 @@ var (
 	errClientClosed      = perrors.New("client closed")
 	errClientReadTimeout = perrors.New("maybe the client read timeout or fail to decode tcp stream in Writer.Write")
 
-	clientConf *ClientConfig
+	clientConf *ClientConfig = GetDefaultClientConfig()
 
 	clientGrPool gxsync.GenericTaskPool
 )
 
 // it is init client for single protocol.
 func initClient(protocol string) {
-	clientConf = GetDefaultClientConfig()
+
 	if protocol == "" {
 		return
 	}
