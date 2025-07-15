c3f1b0c761d1d924b2fff89d87fc86122dce50df
marsevilspirit <marsevilspirit@gmail.com>
2025-07-15 16:03:48 +0800

    docs(converter): add comments

--- tools/protoc-gen-triple-openapi/internal/converter/convert.go
+++ tools/protoc-gen-triple-openapi/internal/converter/convert.go
@@ -89,6 +89,7 @@ func convert(req *pluginpb.CodeGeneratorRequest) (*pluginpb.CodeGeneratorRespons
 			Description: "dubbo-go generate OpenAPI docs.",
 		}
 
+		// handle openapi servers
 		openapiDoc.Servers = append(openapiDoc.Servers, &openapimodel.Server{
 			URL:         "http://0.0.0.0:20000",
 			Description: "Dubbo-go Default Server",
