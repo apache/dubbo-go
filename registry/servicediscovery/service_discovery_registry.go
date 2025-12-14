fca41e95a5e2de9829bca8bf77fd8c9c952ffe04
alanxtl <m134679102365478@163.com>
2025-12-14 12:36:36 +0800

    import format

--- registry/servicediscovery/service_discovery_registry.go
+++ registry/servicediscovery/service_discovery_registry.go
@@ -23,10 +23,16 @@ import (
 	"strings"
 	"sync"
 	"time"
+)
 
+import (
 	gxset "github.com/dubbogo/gost/container/set"
 	"github.com/dubbogo/gost/log/logger"
 
+	perrors "github.com/pkg/errors"
+)
+
+import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
 	"dubbo.apache.org/dubbo-go/v3/common/extension"
@@ -35,13 +41,9 @@ import (
 	"dubbo.apache.org/dubbo-go/v3/metadata/mapping"
 	"dubbo.apache.org/dubbo-go/v3/metadata/report"
 	"dubbo.apache.org/dubbo-go/v3/metrics"
-	perrors "github.com/pkg/errors"
-
 	metricsMetadata "dubbo.apache.org/dubbo-go/v3/metrics/metadata"
-
 	metricsRegistry "dubbo.apache.org/dubbo-go/v3/metrics/registry"
 	"dubbo.apache.org/dubbo-go/v3/registry"
-
 	_ "dubbo.apache.org/dubbo-go/v3/registry/servicediscovery/customizer"
 )
 
