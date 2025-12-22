c2989bdc2e478c3c13da7257755b0ed7c4435a7a
zbchi <1662308219@qq.com>
2025-12-22 14:12:29 +0800

    test: add unit test for otel/trace (#3123)
    * test: add unit test for otel/trace
    
    * format code
    
    * add shutdown

---
+++ otel/trace/stdout/exporter_test.go
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package stdout
+
+import (
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/otel/trace"
+)
+
+func TestNewStdoutExporter(t *testing.T) {
+	config := &trace.ExporterConfig{
+		Exporter:    "stdout",
+		SampleMode:  "always",
+		Propagator:  "w3c",
+		ServiceName: "test-service",
+	}
+
+	exporter, err := newStdoutExporter(config)
+	assert.NoError(t, err)
+	assert.NotNil(t, exporter)
+	assert.NotNil(t, exporter.GetTracerProvider())
+	assert.NotNil(t, exporter.GetPropagator())
+}
+
+func TestNewStdoutExporter_WithConfig(t *testing.T) {
+	config := &trace.ExporterConfig{
+		Exporter:       "stdout",
+		SampleMode:     "ratio",
+		SampleRatio:    0.5,
+		Propagator:     "b3",
+		ServiceName:    "test-service",
+		ServiceVersion: "1.0.0",
+	}
+
+	exporter, err := newStdoutExporter(config)
+	assert.NoError(t, err)
+	assert.NotNil(t, exporter)
+	assert.NotNil(t, exporter.GetTracerProvider())
+	assert.NotNil(t, exporter.GetPropagator())
+}
