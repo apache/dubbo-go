f12040cf515f3b685d461d1116edf799a3cf9827
hana <akashisang@163.com>
2025-12-23 15:07:01 +0800

    fix

--- protocol/invocation/rpcinvocation_test.go
+++ protocol/invocation/rpcinvocation_test.go
@@ -18,6 +18,7 @@
 package invocation
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -28,6 +29,7 @@ import (
 import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/protocol/base"
 )
 
 func TestRPCInvocation_ServiceKey(t *testing.T) {
@@ -72,4 +74,132 @@ func TestRPCInvocation_ServiceKey(t *testing.T) {
 		constant.VersionKey:   diffInfPathConsumerUrl.GetParam(constant.VersionKey, ""),
 	}))
 	assert.Equal(t, providerUrl.ServiceKey(), invocation.ServiceKey())
+}
+
+func TestNewRPCInvocation(t *testing.T) {
+	methodName := "testMethod"
+	arguments := []any{"arg1", 123}
+	attachments := map[string]any{"key1": "value1"}
+
+	inv := NewRPCInvocation(methodName, arguments, attachments)
+
+	assert.Equal(t, methodName, inv.MethodName())
+	assert.Equal(t, arguments, inv.Arguments())
+	assert.Equal(t, attachments, inv.Attachments())
+	assert.NotNil(t, inv.Attributes())
+}
+
+func TestNewRPCInvocationWithOptions(t *testing.T) {
+	t.Run("basic options", func(t *testing.T) {
+		inv := NewRPCInvocationWithOptions(
+			WithMethodName("testMethod"),
+			WithArguments([]any{"arg1", 123}),
+			WithAttachments(map[string]any{"key1": "value1"}),
+			WithReply("testReply"),
+		)
+
+		assert.Equal(t, "testMethod", inv.MethodName())
+		assert.Equal(t, []any{"arg1", 123}, inv.Arguments())
+		assert.Equal(t, "testReply", inv.Reply())
+		assert.NotNil(t, inv.Attributes())
+
+		val, ok := inv.GetAttachment("key1")
+		assert.True(t, ok)
+		assert.Equal(t, "value1", val)
+	})
+
+	t.Run("parameter types", func(t *testing.T) {
+		paramTypes := []reflect.Type{reflect.TypeOf(""), reflect.TypeOf(0)}
+		paramTypeNames := []string{"string", "int"}
+
+		inv := NewRPCInvocationWithOptions(
+			WithParameterTypes(paramTypes),
+			WithParameterTypeNames(paramTypeNames),
+		)
+
+		assert.Equal(t, paramTypes, inv.ParameterTypes())
+		assert.Equal(t, paramTypeNames, inv.ParameterTypeNames())
+	})
+
+	t.Run("with invoker", func(t *testing.T) {
+		invoker := base.NewBaseInvoker(nil)
+		inv := NewRPCInvocationWithOptions(
+			WithInvoker(invoker),
+		)
+
+		assert.Equal(t, invoker, inv.Invoker())
+	})
+
+	t.Run("attachment operations", func(t *testing.T) {
+		inv := NewRPCInvocationWithOptions(
+			WithAttachment("key1", "value1"),
+			WithAttachment("key2", 456),
+		)
+
+		// Test GetAttachment
+		val, ok := inv.GetAttachment("key1")
+		assert.True(t, ok)
+		assert.Equal(t, "value1", val)
+
+		// Test GetAttachmentInterface
+		val2 := inv.GetAttachmentInterface("key2")
+		assert.Equal(t, 456, val2)
+
+		// Test GetAttachmentWithDefaultValue
+		val3 := inv.GetAttachmentWithDefaultValue("nonexistent", "default")
+		assert.Equal(t, "default", val3)
+
+		// Test SetAttachment
+		inv.SetAttachment("key3", "value3")
+		val4, ok := inv.GetAttachment("key3")
+		assert.True(t, ok)
+		assert.Equal(t, "value3", val4)
+	})
+
+	t.Run("attribute operations", func(t *testing.T) {
+		inv := NewRPCInvocationWithOptions(
+			WithMethodName("test"),
+		)
+
+		// Test SetAttribute
+		inv.SetAttribute("attr1", "attrValue1")
+		inv.SetAttribute("attr2", 789)
+
+		// Test GetAttribute
+		attrVal, ok := inv.GetAttribute("attr1")
+		assert.True(t, ok)
+		assert.Equal(t, "attrValue1", attrVal)
+
+		// Test GetAttributeWithDefaultValue
+		attrVal2 := inv.GetAttributeWithDefaultValue("nonexistent", "defaultAttr")
+		assert.Equal(t, "defaultAttr", attrVal2)
+
+		// Test Attributes
+		attributes := inv.Attributes()
+		assert.Len(t, attributes, 2)
+	})
+
+	t.Run("nil attachments initialization", func(t *testing.T) {
+		inv := NewRPCInvocationWithOptions()
+
+		// SetAttachment should initialize nil map
+		assert.NotPanics(t, func() {
+			inv.SetAttachment("key1", "value1")
+		})
+		val, ok := inv.GetAttachment("key1")
+		assert.True(t, ok)
+		assert.Equal(t, "value1", val)
+	})
+
+	t.Run("nil attributes initialization", func(t *testing.T) {
+		inv := NewRPCInvocationWithOptions()
+
+		// SetAttribute should initialize nil map
+		assert.NotPanics(t, func() {
+			inv.SetAttribute("attr1", "value1")
+		})
+		attrVal, ok := inv.GetAttribute("attr1")
+		assert.True(t, ok)
+		assert.Equal(t, "value1", attrVal)
+	})
 }
