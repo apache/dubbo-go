2dbaed75e2895c2d82104e0a5c3173e1f16126db
Snow-kal <1820661379@qq.com>
2026-01-19 13:42:18 +0800

    feat(key): add  a const generic.include.class

--- common/constant/key.go
+++ common/constant/key.go
@@ -417,6 +417,7 @@ const (
 	GenericSerializationGson         = "gson"
 	GenericSerializationProtobuf     = "protobuf"
 	GenericSerializationProtobufJson = "protobuf-json"
+	GenericIncludeClassKey           = "generic.include.class"
 )
 
 // AdaptiveService Filter
