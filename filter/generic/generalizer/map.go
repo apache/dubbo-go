4aab1ead240d2f6ea30098d2b2e0b7ec5bcd14d9
翎 <1820661379@qq.com>
2026-01-19 15:40:10 +0800

    style: changed for CI

--- filter/generic/generalizer/map.go
+++ filter/generic/generalizer/map.go
@@ -27,15 +27,15 @@ import (
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
-	"github. com/dubbogo/gost/log/logger"
+	"github.com/dubbogo/gost/log/logger"
 	"github.com/mitchellh/mapstructure"
 	perrors "github.com/pkg/errors"
 )
 
 import (
 	"dubbo.apache.org/dubbo-go/v3/common/config"
-	"dubbo. apache.org/dubbo-go/v3/common/constant"
-	"dubbo.apache. org/dubbo-go/v3/protocol/dubbo/hessian2"
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/protocol/dubbo/hessian2"
 )
 
 var (
