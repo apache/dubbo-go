22d75d3813d9e7b4d0d6c0dc54a00e859a4a8b32
Snow-kal <1820661379@qq.com>
2026-02-06 23:12:46 +0800

    feat: add global and remove config

--- remoting/getty/getty_client_test.go
+++ remoting/getty/getty_client_test.go
@@ -38,7 +38,6 @@ import (
 import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
-	"dubbo.apache.org/dubbo-go/v3/global"
 	"dubbo.apache.org/dubbo-go/v3/protocol/base"
 	"dubbo.apache.org/dubbo-go/v3/protocol/invocation"
 	"dubbo.apache.org/dubbo-go/v3/protocol/result"
