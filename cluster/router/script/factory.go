77428fe0f84d829092c9bd457c81e8dd0ffd55b5
yangpixi <63222549+yangpixi@users.noreply.github.com>
2026-02-16 17:00:00 +0800

    feat(router): enable register of script router, remove useless comment (#3215)

--- cluster/router/script/factory.go
+++ cluster/router/script/factory.go
@@ -20,16 +20,12 @@ package script
 import (
 	"dubbo.apache.org/dubbo-go/v3/cluster/router"
 	"dubbo.apache.org/dubbo-go/v3/common"
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/common/extension"
 )
 
 func init() {
-	/*
-		Tag router is not imported in dubbo-go/imports/imports.go, because it relies on config center,
-		and cause warning if config center is empty.
-		User can import this package and config config center to use Script router.
-	*/
-	// TODO(finalt) Temporarily removed until fixed (https://github.com/apache/dubbo-go/pull/2716)
-	//extension.SetRouterFactory(constant.ScriptRouterFactoryKey, NewScriptRouterFactory)
+	extension.SetRouterFactory(constant.ScriptRouterFactoryKey, NewScriptRouterFactory)
 }
 
 // ScriptRouteFactory router factory
