14dc521e03556980b6c32ee443f9d8fa39f91019
CAICAIIs <3360776475@qq.com>
2026-02-18 00:29:16 +0800

    fix: update test templates to use programmatic API
    Update go-client/cmd/client.go and go-server/cmd/server.go in
    testGenCode/template/newDemo/ to match the new config-free approach:
    
    - Replace config.SetConsumerService() + config.Load() with client.NewClient()
    - Replace config.SetProviderService() + config.Load() with server.NewServer()
    - Use api.NewGreeterClient(cli) instead of grpcGreeterImpl
    - Use api.RegisterGreeterServer(srv, provider) instead of config registration
    - Remove DUBBO_GO_CONFIG_PATH environment variable comments
    
    This aligns the test templates with the generator implementation,
    fixing the TestNewDemo test failure.
    
    Related: #3204

--- tools/dubbogo-cli/cmd/testGenCode/template/newDemo/go-server/cmd/server.go
+++ tools/dubbogo-cli/cmd/testGenCode/template/newDemo/go-server/cmd/server.go
@@ -27,8 +27,9 @@ import (
 
 import (
 	"dubbo.apache.org/dubbo-go/v3/common/logger"
-	"dubbo.apache.org/dubbo-go/v3/config"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+	"dubbo.apache.org/dubbo-go/v3/server"
 )
 
 type GreeterProvider struct {
@@ -40,11 +41,20 @@ func (s *GreeterProvider) SayHello(ctx context.Context, in *api.HelloRequest) (*
 	return &api.User{Name: "Hello " + in.Name, Id: "12345", Age: 21}, nil
 }
 
-// export DUBBO_GO_CONFIG_PATH= PATH_TO_SAMPLES/helloworld/go-server/conf/dubbogo.yaml
 func main() {
-	config.SetProviderService(&GreeterProvider{})
-	if err := config.Load(); err != nil {
+	srv, err := server.NewServer(
+		server.WithServerProtocol(
+			protocol.WithPort(20000),
+			protocol.WithTriple(),
+		),
+	)
+	if err != nil {
 		panic(err)
 	}
-	select {}
+
+	api.RegisterGreeterServer(srv, &GreeterProvider{})
+
+	if err := srv.Serve(); err != nil {
+		logger.Error(err)
+	}
 }
