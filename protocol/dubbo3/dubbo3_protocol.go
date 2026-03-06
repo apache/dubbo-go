929f78b22094b48e6e152c93484eb7c937ccd8a1
Snow-kal <1820661379@qq.com>
2026-03-06 13:47:53 +0800

    style: imports-formatter

--- protocol/dubbo3/dubbo3_protocol.go
+++ protocol/dubbo3/dubbo3_protocol.go
@@ -22,22 +22,27 @@ import (
 	"fmt"
 	"reflect"
 	"sync"
+)
 
+import (
 	"github.com/dubbogo/gost/log/logger"
+
 	"github.com/dubbogo/grpc-go"
 	"github.com/dubbogo/grpc-go/metadata"
 
 	tripleConstant "github.com/dubbogo/triple/pkg/common/constant"
+	triConfig "github.com/dubbogo/triple/pkg/config"
+	"github.com/dubbogo/triple/pkg/triple"
 
+	"github.com/dustin/go-humanize"
+)
+
+import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
 	"dubbo.apache.org/dubbo-go/v3/global"
 	"dubbo.apache.org/dubbo-go/v3/protocol/base"
 	"dubbo.apache.org/dubbo-go/v3/protocol/invocation"
-	triConfig "github.com/dubbogo/triple/pkg/config"
-	"github.com/dubbogo/triple/pkg/triple"
-	"github.com/dustin/go-humanize"
-
 	dubbotls "dubbo.apache.org/dubbo-go/v3/tls"
 )
 
