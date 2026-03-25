5c91e89e5c9d504179a4654d4503d2733aaf9b9d
zb <1662308219@qq.com>
2026-03-25 21:00:22 +0800

    refactor: use RoundTripper rather than Transport

---
+++ protocol/triple/dual_transport.go
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package triple
+
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+
+	tri "dubbo.apache.org/dubbo-go/v3/protocol/triple/triple_protocol"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+	"golang.org/x/net/http2"
+)
+
+// dualTransport is a transport that can handle both HTTP/2 and HTTP/3
+// It uses HTTP Alternative Services (Alt-Svc) for protocol negotiation
+type dualTransport struct {
+	http2Transport http.RoundTripper
+	http3Transport http.RoundTripper
+	// Cache for alternative services to avoid repeated lookups
+	altSvcCache *tri.AltSvcCache
+}
+
+// newDualTransport creates a new dual transport that supports both HTTP/2 and HTTP/3
+func newDualTransport(tlsConfig *tls.Config, keepAliveInterval, keepAliveTimeout time.Duration) http.RoundTripper {
+	http2Transport := &http2.Transport{
+		TLSClientConfig: tlsConfig,
+		ReadIdleTimeout: keepAliveInterval,
+		PingTimeout:     keepAliveTimeout,
+	}
+
+	http3Transport := &http3.Transport{
+		TLSClientConfig: tlsConfig,
+		QUICConfig: &quic.Config{
+			KeepAlivePeriod: keepAliveInterval,
+			MaxIdleTimeout:  keepAliveTimeout,
+		},
+	}
+
+	return &dualTransport{
+		http2Transport: http2Transport,
+		http3Transport: http3Transport,
+		altSvcCache:    tri.NewAltSvcCache(),
+	}
+}
+
+// RoundTrip implements http.RoundTripper interface with HTTP Alternative Services support
+func (dt *dualTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+}
