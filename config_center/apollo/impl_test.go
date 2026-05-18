6dbf9a94dd86fedc92bebbc3616caeb30291cb2e
bang <3656828039@qq.com>
2026-05-18 19:52:42 +0800

    refator(config_center):cleanup-redundant-test

--- config_center/apollo/impl_test.go
+++ config_center/apollo/impl_test.go
@@ -237,13 +237,6 @@ func TestListener(t *testing.T) {
 	assert.Equal(t, 0, listenerCount)
 }
 
-func TestGetAddressWithProtocolPrefixKeepsContext(t *testing.T) {
-	apolloCfgURL, err := common.NewURL("apollo://127.0.0.1:8080/config", common.WithProtocol("apollo"))
-	require.NoError(t, err)
-	config := &apolloConfiguration{}
-	assert.Equal(t, "http://127.0.0.1:8080/config", config.getAddressWithProtocolPrefix(apolloCfgURL))
-}
-
 type apolloDataListener struct {
 	wg    sync.WaitGroup
 	count int
