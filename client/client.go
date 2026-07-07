99e6220145973cc86ebab9bcb8a7c89406e329e3
alanxtl <m134679102365478@163.com>
2026-07-07 11:27:10 +0800

    FMT

--- client/client.go
+++ client/client.go
@@ -21,7 +21,9 @@ package client
 import (
 	"context"
 	"errors"
+)
 
+import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
 	"dubbo.apache.org/dubbo-go/v3/filter/generic"
